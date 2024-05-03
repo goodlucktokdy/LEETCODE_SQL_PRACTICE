6157edd588df0f24f237534a84625ba600dc4458
goodlucktokdy <148170828+goodlucktokdy@users.noreply.github.com>
2024-05-04 00:02:02 +0900

    Time: 932 ms (99.61%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,9 +1,30 @@
-select d.name Department, e.name Employee, e.salary Salary
-from Employee e
-inner join
-Department d
-on
-    e.departmentId = d.id
-where
-    (e.departmentId, salary) in (select departmentId, max(salary) from Employee
-                group by departmentId)
+-- select d.name Department, e.name Employee, e.salary Salary
+-- from Employee e
+-- inner join
+-- Department d
+-- on
+--     e.departmentId = d.id
+-- where
+--     (e.departmentId, salary) in (select departmentId, max(salary) from Employee
+--                 group by departmentId)
+with t1 as
+    (
+    select distinct 
+        d.name Department, 
+        (
+            case 
+                when rank() over (partition by departmentId order by salary desc) = 1 then e.name
+                end
+        ) as Employee, 
+        first_value(salary) over (partition by departmentId order by salary desc) Salary
+    from 
+        Employee e
+    inner join
+        Department d on e.departmentId = d.id
+    )
+    select 
+        Department, Employee, Salary
+    from
+        t1 
+    where
+        Employee is not null
