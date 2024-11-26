5181583761082a04d59ff125440923526c12f8d4
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-11-26 22:04:43 +0900

    Time: 1496 ms (12.29%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -0,0 +1,36 @@
+# Write your MySQL query statement below
+with base as (
+    select 
+        a.sale_id,
+        a.product_id,
+        b.product_name,
+        a.year,
+        a.quantity,
+        a.price
+    from 
+        Sales a
+    left join 
+        Product b 
+    on 
+        a.product_id = b.product_id
+)
+select 
+    distinct
+    a.product_id,
+    a.first_year,
+    a.quantity,
+    a.price
+from (
+    select 
+        product_id,
+        year,
+        min(year) over (partition by product_id) as first_year,
+        quantity,
+        price
+    from
+        base
+    group by 
+        product_id, quantity, price
+) a
+where 
+    a.year = a.first_year
