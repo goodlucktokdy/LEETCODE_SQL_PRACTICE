4448c36ae215da80088900ad4d2851cc3bb3a51e
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-11-26 21:24:39 +0900

    Time: 275 ms (78.91%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,29 +1,12 @@
--- # Write your MySQL query statement below
--- with base as (
---     select 
---         id,
---         case when a.ranks = 2 and count(a.ranks) = 1 then a.salary
---         else null end as SecondHighestSalary
---     from (
---         select 
---             id,
---             salary,
---             rank() over (order by salary desc) as ranks
---         from 
---             Employee
---     ) a
---     group by 
---         1
--- )
--- select 
---     SecondHighestSalary
--- from 
---     base
--- order by 
---     SecondHighestSalary desc 
--- limit 1
-select
-(select distinct Salary 
-from Employee order by salary desc 
-limit 1 offset 1) 
-as SecondHighestSalary;
+# Write your MySQL query statement below
+select 
+    (
+select 
+    distinct
+    salary
+from 
+    Employee
+order by 
+    salary desc
+limit 1 offset 1
+) as SecondHighestSalary
