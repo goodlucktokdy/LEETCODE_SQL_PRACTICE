9e9483b63049f4f5b94a91500318b1800e8ae8bb
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-11-27 18:03:53 +0900

    Time: 3106 ms (5.02%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+select 
+    query_name,
+    round(avg(rating/position),2) as quality,
+    round(coalesce(100*count(case when rating < 3 then 1 else null end)/count(query_name),0),2) as poor_query_percentage
+from 
+    Queries
+where 
+    query_name is not null
+group by 
+    query_name
