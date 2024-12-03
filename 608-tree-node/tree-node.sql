95e7c5899daa4e1e8d2aafd42d9f340d457bc804
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-12-03 14:28:47 +0900

    Time: 612 ms (20.96%) | Memory: 0B (100.00%) - LeetSync

--- 608-tree-node/tree-node.sql
+++ 608-tree-node/tree-node.sql
@@ -1,46 +1,9 @@
-with recursive cte as (
-    select
-        id,
-        p_id,
-        1 as gen
-    from 
-        Tree
-    where 
-        p_id is null
-    union all
-    select
-        a.id,
-        a.p_id,
-        cte.gen + 1
-    from 
-        Tree a
-    inner join
-        cte
-    on
-        cte.id = a.p_id
-), leaves as (
-    select
-        a.aid
-    from (
-        select
-            a.id as aid,
-            a.p_id as apid,
-            b.id as bid,
-            b.p_id as bpid
-        from 
-            cte a 
-        left join 
-            cte b 
-        on 
-            a.id = b.p_id
-        where 
-            b.p_id is null
-    ) a
-)
 select
     id,
-    case when gen = 1 then 'Root'
-        when id in (select * from leaves) then 'Leaf'
-        else 'Inner' end as type
+    case when p_id is null then 'Root'
+        when id in (select p_id from tree) then 'Inner'
+        else 'Leaf' end as type
 from 
-    cte
+    tree
+order by 
+    id
