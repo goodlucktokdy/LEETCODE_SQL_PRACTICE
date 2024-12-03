0b665e807f6123906eb12747f87ac9307ea272b5
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-12-03 18:35:43 +0900

    Time: 408 ms (48.90%) | Memory: 0B (100.00%) - LeetSync

--- 1136-actors-and-directors-who-cooperated-at-least-three-times/actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 1136-actors-and-directors-who-cooperated-at-least-three-times/actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,17 +1,9 @@
-with base as (
-    select
-        concat(actor_id,',',director_id) as pairs,
-        count(timestamp) as cnts
-    from 
-        ActorDirector
-    group by 
-        pairs
-    having
-        cnts >= 3
-)
 select
-    distinct
-    cast(substring_index(pairs,',',1) as real) as actor_id,
-    cast(substring_index(pairs,',',-1) as real) as director_id
+    actor_id,
+    director_id
 from 
-    base
+    ActorDirector
+group by 
+    1,2
+having 
+    count(timestamp) >= 3
