bb99c805001b3945dec141d34d0c5409736d02ea
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-12-03 19:06:24 +0900

    Time: 839 ms (37.26%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,30 +1,27 @@
--- select d.name Department, e.name Employee, e.salary Salary
--- from Employee e
--- inner join
--- Department d
--- on
---     e.departmentId = d.id
--- where
---     (e.departmentId, salary) in (select departmentId, max(salary) from Employee
---                 group by departmentId)
-with t1 as
-    (
-    select distinct 
-        d.name Department, 
-        (
-            case 
-                when rank() over (partition by departmentId order by salary desc) = 1 then e.name
-                end
-        ) as Employee, 
-        first_value(salary) over (partition by departmentId order by salary desc) Salary
+with base as (
+    select
+        b.name as Department,
+        a.name as Employee,
+        a.salary
     from 
-        Employee e
-    inner join
-        Department d on e.departmentId = d.id
-    )
-    select 
-        Department, Employee, Salary
-    from
-        t1 
-    where
-        Employee is not null
+        Employee a
+    left join
+        Department b
+    on 
+        b.id = a.departmentId
+)
+select
+    a.Department,
+    a.Employee,
+    a.Salary
+from (
+    select
+        Department,
+        Employee,
+        Salary,
+        dense_rank() over (partition by Department order by salary desc) as ranks
+    from 
+        base
+) a
+where 
+    ranks = 1
