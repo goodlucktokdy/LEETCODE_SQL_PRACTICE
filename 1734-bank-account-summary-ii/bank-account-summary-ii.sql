c7d8a0fd9f06dc6cf4b40ed3430a6460e5229628
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-12-03 18:43:44 +0900

    Time: 795 ms (79.77%) | Memory: 0B (100.00%) - LeetSync

--- 1734-bank-account-summary-ii/bank-account-summary-ii.sql
+++ 1734-bank-account-summary-ii/bank-account-summary-ii.sql
@@ -1,34 +1,21 @@
--- with base as (
---     select
---         a.account,
---         a.name,
---         b.amount
---     from 
---         Users a 
---     inner join
---         Transactions b
---     on
---         a.account = b.account
--- )
--- select
---     name,
---     sum(amount) as balance
--- from 
---     base
--- group by 
---     name
--- having
---     balance >= 10000
-SELECT 
-    DISTINCT a.name, b.balance
-FROM 
-    Users a
-JOIN (
-    SELECT 
-        account, SUM(amount) as balance
-    FROM 
-        Transactions
-    GROUP BY 1
-    HAVING balance>10000) b
-ON 
-    a.account = b.account 
+with base as (
+    select
+        a.account,
+        a.name,
+        b.amount
+    from 
+        Users a 
+    inner join
+        Transactions b
+    on
+        a.account = b.account
+)
+select
+    name,
+    sum(amount) as balance
+from 
+    base
+group by 
+    name
+having
+    balance > 10000
