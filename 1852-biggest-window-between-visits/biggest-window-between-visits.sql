2a5e63d596493bc7bebada9d9bdc12c6c814b7eb
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-12-03 13:01:49 +0900

    Time: 503 ms (92.34%) | Memory: 0B (100.00%) - LeetSync

--- 1852-biggest-window-between-visits/biggest-window-between-visits.sql
+++ 1852-biggest-window-between-visits/biggest-window-between-visits.sql
@@ -1,18 +1,17 @@
 with base as (
     select
         user_id,
-        lag(visit_date) over (partition by user_id order by visit_date) as prev_visit,
         visit_date,
-        timestampdiff(day,lag(visit_date) over (partition by user_id order by visit_date),visit_date) as diff_of_days,
-        timestampdiff(day,max(visit_date) over (partition by user_id),'2021-01-01') as diff_today 
+        lead(visit_date) over (partition by user_id order by visit_date) as next_visit
     from 
         UserVisits
 )
 select
     user_id,
-    max(case when coalesce(diff_of_days,0) > diff_today then diff_of_days 
-        else diff_today end) as biggest_window
+    max(timestampdiff(day,visit_date,ifnull(next_visit,'2021-01-01'))) as biggest_window
 from 
     base
 group by 
+    user_id
+order by 
     user_id
