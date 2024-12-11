572f6906c2cafe18d68ff2823c6695bdfba95263
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-12-11 21:34:18 +0900

    Time: 704 ms (66.89%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1328-monthly-transactions-ii/monthly-transactions-ii.sql
@@ -0,0 +1,42 @@
+with base as (
+    select 
+        date_format(a.trans_date,'%Y-%m') as month,
+        b.country,
+        0 as approved_count,
+        0 as approved_amount,
+        count(distinct a.trans_id) as chargeback_count,
+        sum(b.amount) as chargeback_amount
+    from 
+        Chargebacks a 
+    join 
+        Transactions b
+    on 
+        a.trans_id = b.id
+    group by 
+        1,2
+    union all
+    select 
+        date_format(trans_date,'%Y-%m') as month,
+        country,
+        count(id) as approved_count,
+        sum(amount) as approved_amount,
+        0 as chargeback_count,
+        0 as chargeback_amount
+    from
+        Transactions 
+    where 
+        state = 'approved'
+    group by 
+        1,2
+)
+select 
+    month,
+    country,
+    sum(approved_count) as approved_count,
+    sum(approved_amount) as approved_amount,
+    sum(chargeback_count) as chargeback_count,
+    sum(chargeback_amount) as chargeback_amount
+from 
+    base 
+group by 
+    1,2
