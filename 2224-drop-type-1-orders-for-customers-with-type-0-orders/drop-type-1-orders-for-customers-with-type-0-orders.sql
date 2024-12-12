2f9d126a3fe24e6e65496414ceb8d9f8e75ef071
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-12-12 16:46:16 +0900

    Time: 744 ms (9.18%) | Memory: 0B (100.00%) - LeetSync

--- 2224-drop-type-1-orders-for-customers-with-type-0-orders/drop-type-1-orders-for-customers-with-type-0-orders.sql
+++ 2224-drop-type-1-orders-for-customers-with-type-0-orders/drop-type-1-orders-for-customers-with-type-0-orders.sql
@@ -1,22 +1,13 @@
 # Write your MySQL query statement below
 with base as (
     select 
-        distinct
-        a.customer_id,
-        a.order_id,
-        a.order_type,
-        a.type0,
-        a.type1
-    from (
-        select 
-            order_id,
-            customer_id,
-            order_type,
-            count(case when order_type = 0 then order_id else null end) over (partition by customer_id) as type0,
-            count(case when order_type = 1 then order_id else null end) over (partition by customer_id) as type1
-        from 
-            Orders
-    ) a
+        order_id,
+        customer_id,
+        order_type,
+        count(case when order_type = 0 then order_id else null end) over (partition by customer_id) as type0,
+        count(case when order_type = 1 then order_id else null end) over (partition by customer_id) as type1
+    from 
+        Orders
 )
 select 
     distinct
