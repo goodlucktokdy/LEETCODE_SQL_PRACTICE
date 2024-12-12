ceb0c561289675aa52c7868e1fc2707247a63c62
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2024-12-12 14:01:47 +0900

    Time: 410 ms (17.47%) | Memory: 0B (100.00%) - LeetSync

--- 3285-manager-of-the-largest-department/manager-of-the-largest-department.sql
+++ 3285-manager-of-the-largest-department/manager-of-the-largest-department.sql
@@ -1,29 +1,24 @@
 # Write your MySQL query statement below
 with base as (
     select 
-        *,
-        dense_rank() over (order by a.dept_size desc) as ranks
-    from (
-        select 
-            count(emp_id) over (partition by dep_id) as dept_size,
-            emp_id,
-            dep_id,
-            position
-        from 
-            Employees
-    ) a
+        dep_id,
+        dense_rank() over (order by count(*) desc) as ranks
+    from 
+        Employees
+    group by 
+        dep_id
 )
 select 
     distinct
     b.emp_name as manager_name,
     a.dep_id
 from 
-    base a 
+    base a
 inner join 
     Employees b 
 on 
-    a.emp_id = b.emp_id
+    a.dep_id = b.dep_id
 where 
-    a.ranks = 1 and a.position = 'Manager'
+    a.ranks = 1 and b.position = 'Manager'
 order by 
     a.dep_id asc
