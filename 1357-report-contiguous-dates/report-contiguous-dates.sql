93fe215c9d526ffbdeecdd20b1606d623f088e30
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-03 13:52:00 +0900

    Time: 2033 ms (49.32%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1357-report-contiguous-dates/report-contiguous-dates.sql
@@ -0,0 +1,32 @@
+
+with base as (
+    select 
+        'failed' as period_state,
+        fail_date as dates
+    from 
+        Failed
+    union all
+    select 
+        'succeeded',
+        success_date
+    from 
+        Succeeded
+)
+select 
+    period_state,
+    min(dates) as start_date,
+    max(dates) as end_date
+from (
+    select 
+        period_state,
+        dates,
+        date_sub(dates,interval row_number() over (partition by period_state order by dates) day) as sess
+    from 
+        base
+    where 
+        year(dates) = 2019
+) a 
+group by 
+    period_state, sess
+order by 
+    start_date
