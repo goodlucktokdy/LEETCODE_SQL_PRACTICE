9f567257e287a23ea210130925ea34438e58d8c4
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-03 16:28:36 +0900

    Time: 3591 ms (18.49%) | Memory: 0B (100.00%) - LeetSync

--- 1795-hopper-company-queries-iii/hopper-company-queries-iii.sql
+++ 1795-hopper-company-queries-iii/hopper-company-queries-iii.sql
@@ -1,47 +1,48 @@
+# Write your MySQL query statement below
 with recursive cte as (
     select 
         1 as month
     union all
     select 
         cte.month + 1 
-    from    
+    from 
         cte 
-    where   
-        cte.month < 12
-), 
-final_info as (
+    where 
+        cte.month < 12 
+),
+base as (
+    select 
+        month(a.requested_at) as month,
+        sum(b.ride_distance) as ride_distance,
+        sum(b.ride_duration) as ride_duration
+    from 
+        Rides a 
+    inner join 
+        AcceptedRides b 
+    on 
+        a.ride_id = b.ride_id 
+    where 
+        year(a.requested_at) = 2020
+    group by 
+        month
+)
+select 
+    month,
+    average_ride_distance,
+    average_ride_duration
+from (
     select 
         a.month as month,
         round(avg(coalesce(b.ride_distance,0)) over (order by a.month range between 0 preceding and 2 following),2) as average_ride_distance,
         round(avg(coalesce(b.ride_duration,0)) over (order by a.month range between 0 preceding and 2 following),2) as average_ride_duration
     from 
         cte a 
-    left join (
-        select 
-            month(a.requested_at) as month,
-            sum(b.ride_distance) as ride_distance,
-            sum(b.ride_duration) as ride_duration
-        from 
-            Rides a 
-        inner join 
-            AcceptedRides b 
-        on 
-            a.ride_id = b.ride_id
-        where 
-            year(a.requested_at) = 2020
-        group by 
-            month 
-    ) b 
+    left join
+        base b 
     on 
-        a.month = b.month 
-)
-select 
-    month,
-    average_ride_distance,
-    average_ride_duration
-from 
-    final_info 
+        a.month = b.month
+) a 
 where 
-    month <= 10 
+    month <= 10
 order by 
-    month 
+    month
