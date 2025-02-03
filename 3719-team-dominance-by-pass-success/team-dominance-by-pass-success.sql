ccaf788f3c39fc7f822be1f6053d808d0529ef6b
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-03 18:46:59 +0900

    Time: 1071 ms (24.37%) | Memory: 0B (100.00%) - LeetSync

--- 3719-team-dominance-by-pass-success/team-dominance-by-pass-success.sql
+++ 3719-team-dominance-by-pass-success/team-dominance-by-pass-success.sql
@@ -1,12 +1,14 @@
+# Write your MySQL query statement below
 with base as (
-    select 
-        a.team_name as from_team,
-        cast(replace(b.time_stamp,':','.') as real) as time_stamp,
-        c.team_name as to_team
+    select
+        a.team_name as pass_from,
+        case when cast(replace(b.time_stamp,':','.') as real) <= 45 then 1 else 2 end as half_number,
+        c.team_name as pass_to,
+        if(a.team_name = c.team_name,1,-1) as points
     from 
         Teams a 
-    inner join 
-        Passes b 
+    inner join
+        Passes b
     on 
         a.player_id = b.pass_from
     inner join 
@@ -15,9 +17,9 @@ with base as (
         b.pass_to = c.player_id
 )
 select 
-    from_team as team_name,
-    case when time_stamp <= 45 then 1 else 2 end as half_number,
-    sum(case when from_team = to_team then 1 else -1 end) as dominance
+    pass_from as team_name,
+    half_number,
+    sum(points) as dominance
 from 
     base 
 group by 
