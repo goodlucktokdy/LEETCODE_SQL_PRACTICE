9fa6abd5ad8b91aab700ea0f7d6eec302dcdac23
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-03 13:48:41 +0900

    Time: 367 ms (60.90%) | Memory: 0B (100.00%) - LeetSync

---
+++ 571-find-median-given-frequency-of-numbers/find-median-given-frequency-of-numbers.sql
@@ -0,0 +1,32 @@
+with recursive cte as (
+    select 
+        num,
+        1 as frequency
+    from 
+        Numbers
+    union all
+    select 
+        a.num,
+        cte.frequency + 1
+    from 
+        Numbers a
+    inner join 
+        cte 
+    on 
+        a.num = cte.num
+    and 
+        cte.frequency < a.frequency 
+)
+select 
+    round(avg(num),1) as median
+from (
+    select 
+        num,
+        frequency,
+        row_number() over (order by num) as rnums,
+        count(num) over () as cnts 
+    from 
+        cte
+) a 
+where 
+    rnums between cnts/2 and cnts/2 + 1
