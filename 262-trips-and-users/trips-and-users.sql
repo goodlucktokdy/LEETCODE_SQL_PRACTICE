7e87698c1f1923fb0f39e31469e21c246a81dfa7
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-03 13:47:38 +0900

    Time: 1138 ms (28.71%) | Memory: 0B (100.00%) - LeetSync

--- 262-trips-and-users/trips-and-users.sql
+++ 262-trips-and-users/trips-and-users.sql
@@ -1,19 +1,12 @@
-with t1 as
-    (select a.id, a.status, a.request_at,
-            b.banned as client_ban, c.banned as driver_ban
-        from Trips a
-        inner join
-        Users b
-        on a.client_id = b.users_id
-        inner join
-        Users c
-        on a.driver_id = c.users_id
-        where b.banned = 'No' and c.banned = 'No'
-    ),
-t2 as (select request_at Day,
-                round((count(case when status != 'completed' then id end)/count(id)),2) 'Cancellation Rate'
-                from t1
-            where request_at between date('2013-10-01') and date('2013-10-03')
-            group by request_at
-)
-select * from t2
+select 
+    date(request_at) as Day,
+    round(count(distinct case when status like '%cancel%' then id end)/count(distinct id),2) as 'Cancellation Rate'
+from 
+    Trips t 
+where not exists 
+    (select 1 from Users u 
+        where u.banned = 'Yes' and (t.client_id = u.users_id or t.driver_id = u.users_id))
+and 
+    date(request_at) between '2013-10-01' and '2013-10-03'
+group by 
+    Day 
