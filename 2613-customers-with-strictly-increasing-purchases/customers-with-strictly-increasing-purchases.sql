b9c4a6e33b7e606e5054863db2f3ba2beeb9a8fb
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-03 17:41:28 +0900

    Time: 1848 ms (6.25%) | Memory: 0B (100.00%) - LeetSync

--- 2613-customers-with-strictly-increasing-purchases/customers-with-strictly-increasing-purchases.sql
+++ 2613-customers-with-strictly-increasing-purchases/customers-with-strictly-increasing-purchases.sql
@@ -1,28 +1,30 @@
+# Write your MySQL query statement below
 with base as (
     select 
-        year(order_date) as years,
         customer_id,
+        year(order_date) as years,
         sum(price) as total_sales
     from 
         Orders 
     group by 
-        years, customer_id
+        customer_id, years
 ),
 prev_info as (
     select 
         customer_id,
-        lag(years,1,years) over (partition by customer_id order by years) as prev_years,
+        lag(years,1,years - 1) over (partition by customer_id order by years) as prev_years,
         years,
         lag(total_sales,1,0) over (partition by customer_id order by years) as prev_sales,
         total_sales
     from 
-        base 
+        base
 )
 select 
     distinct 
-    customer_id
+    a.customer_id
 from 
     prev_info a 
 where not exists 
     (select 1 from prev_info b 
-        where a.customer_id = b.customer_id and (b.years - b.prev_years > 1 or b.total_sales - b.prev_sales <= 0))
+        where a.customer_id = b.customer_id and 
+        (b.years - b.prev_years > 1 or b.total_sales <= b.prev_sales))
