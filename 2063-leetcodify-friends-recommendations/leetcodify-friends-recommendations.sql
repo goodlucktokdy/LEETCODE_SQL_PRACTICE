5c6faa16f6602688315b86ba0d3bf774b659ba86
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-03 13:54:44 +0900

    Time: 3008 ms (53.53%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2063-leetcodify-friends-recommendations/leetcodify-friends-recommendations.sql
@@ -0,0 +1,39 @@
+with pairs as (
+    select 
+        user1_id,
+        user2_id
+    from 
+        Friendship
+    union all
+    select 
+        user2_id,
+        user1_id
+    from 
+        Friendship
+),
+base as (
+    select 
+        a.day,
+        a.song_id,
+        a.user_id as user1_id,
+        b.user_id as user2_id
+    from 
+        Listens a 
+    inner join 
+        Listens b 
+    on 
+        a.day = b.day and a.song_id = b.song_id and a.user_id != b.user_id
+)
+select 
+    distinct
+    user1_id as user_id,
+    user2_id as recommended_id
+from 
+    base a 
+where not exists 
+    (select 1 from pairs b 
+        where a.user1_id = b.user1_id and a.user2_id = b.user2_id)
+group by 
+    user_id,recommended_id,day
+having 
+    count(distinct song_id) >= 3
