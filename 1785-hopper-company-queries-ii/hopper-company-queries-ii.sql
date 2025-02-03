143fcbe655908bdffe4625285551e4602d3e2369
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-03 16:21:26 +0900

    Time: 1551 ms (93.15%) | Memory: 0B (100.00%) - LeetSync

--- 1785-hopper-company-queries-ii/hopper-company-queries-ii.sql
+++ 1785-hopper-company-queries-ii/hopper-company-queries-ii.sql
@@ -1,3 +1,4 @@
+# Write your MySQL query statement below
 with recursive cte as (
     select 
         1 as month
@@ -12,17 +13,17 @@ with recursive cte as (
 active_drivers_cte as (
     select 
         a.month as month,
-        sum(coalesce(b.drivers,0)) over (order by a.month) as active_drivers
+        sum(coalesce(b.active_drivers,0)) over (order by a.month) as active_drivers
     from 
         cte a 
     left join (
         select 
             month(case when join_date < '2020-01-01' then '2020-01-01' else join_date end) as month,
-            count(distinct driver_id) as drivers
+            count(distinct driver_id) as active_drivers
         from 
-            Drivers 
+            Drivers
         where 
-            year(join_date) <= 2020
+            join_date <= '2020-12-31'
         group by 
             month
     ) b 
@@ -31,36 +32,36 @@ active_drivers_cte as (
 ),
 accepted_drivers_cte as (
     select 
-        a.month,
-        coalesce(b.drivers,0) as accepted_drivers
-    from  
+        a.month as month,
+        coalesce(b.accepted_drivers,0) as accepted_drivers
+    from 
         cte a 
     left join (
         select 
             month(a.requested_at) as month,
-            count(distinct b.driver_id) as drivers
+            count(distinct b.driver_id) as accepted_drivers
         from 
             Rides a 
         inner join 
             AcceptedRides b 
         on 
-            a.ride_id = b.ride_id 
+            a.ride_id = b.ride_id
         where 
             year(a.requested_at) = 2020
         group by 
             month
     ) b 
     on 
-        a.month = b.month
+        a.month = b.month 
 )
 select 
-    a.month,
-    round(coalesce(100.0*b.accepted_drivers/a.active_drivers,0),2) as working_percentage
+    a.month as month,
+    round(coalesce(100.0*a.accepted_drivers/b.active_drivers,0),2) as working_percentage
 from 
-    active_drivers_cte a 
+    accepted_drivers_cte a 
 inner join 
-    accepted_drivers_cte b 
+    active_drivers_cte b 
 on 
-    a.month = b.month 
+    a.month = b.month
 order by 
-    a.month 
+    month
