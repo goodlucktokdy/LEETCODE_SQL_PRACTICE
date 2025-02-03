54f075058d051a756ce0172818c6604159d24138
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-03 14:05:38 +0900

    Time: 752 ms (18.60%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3520-year-on-year-growth-rate/year-on-year-growth-rate.sql
@@ -0,0 +1,27 @@
+with base as (
+    select 
+        year,
+        product_id,
+        curr_year_spend,
+        lag(curr_year_spend) over (partition by product_id order by year) as prev_year_spend
+    from (
+        select 
+            year(transaction_date) as year,
+            product_id,
+            sum(spend) as curr_year_spend
+        from 
+            user_transactions
+        group by 
+            year, product_id
+    ) a
+)
+select 
+    year,
+    product_id,
+    curr_year_spend,
+    prev_year_spend,
+    round(100.0*(curr_year_spend - prev_year_spend)/prev_year_spend,2) as yoy_rate
+from 
+    base
+order by 
+    product_id, year 
