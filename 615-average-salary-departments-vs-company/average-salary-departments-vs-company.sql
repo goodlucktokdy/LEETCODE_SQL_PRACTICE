8557904e0d92b5e1784da84d78df7f76779971e2
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-03 14:48:56 +0900

    Time: 569 ms (57.01%) | Memory: 0B (100.00%) - LeetSync

--- 615-average-salary-departments-vs-company/average-salary-departments-vs-company.sql
+++ 615-average-salary-departments-vs-company/average-salary-departments-vs-company.sql
@@ -1,13 +1,8 @@
-
-select 
-    distinct
-    pay_month,
-    department_id,
-    case when dept_avg > company_avg then 'higher'
-        when dept_avg < company_avg then 'lower' else 'same' end as comparison
-from (
+# Write your MySQL query statement below
+with salary_info as (
     select 
-        date_format(a.pay_date, '%Y-%m') as pay_month,
+        date_format(a.pay_date,'%Y-%m') as pay_month,
+        a.employee_id,
         b.department_id,
         avg(a.amount) over (partition by date_format(a.pay_date,'%Y-%m'),b.department_id) as dept_avg,
         avg(a.amount) over (partition by date_format(a.pay_date,'%Y-%m')) as company_avg
@@ -17,4 +12,12 @@ from (
         Employee b 
     on 
         a.employee_id = b.employee_id
-) a 
+)
+select 
+    distinct
+    pay_month,
+    department_id,
+    case when dept_avg > company_avg then 'higher'
+        when dept_avg < company_avg then 'lower' else 'same' end as comparison
+from 
+    salary_info
