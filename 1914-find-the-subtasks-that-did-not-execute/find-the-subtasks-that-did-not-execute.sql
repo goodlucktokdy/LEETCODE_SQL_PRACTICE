805f0d08e7539db98bd2fc21d8075c69e4f95ddc
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-03 16:35:30 +0900

    Time: 2385 ms (12.96%) | Memory: 0B (100.00%) - LeetSync

--- 1914-find-the-subtasks-that-did-not-execute/find-the-subtasks-that-did-not-execute.sql
+++ 1914-find-the-subtasks-that-did-not-execute/find-the-subtasks-that-did-not-execute.sql
@@ -1,3 +1,4 @@
+# Write your MySQL query statement below
 with recursive cte as (
     select 
         task_id,
@@ -13,15 +14,18 @@ with recursive cte as (
     inner join 
         cte 
     on 
-        a.task_id = cte.task_id 
+        a.task_id = cte.task_id
     where 
         cte.subtask_id < a.subtasks_count
 )
 select 
-    task_id,
-    subtask_id 
+    a.task_id,
+    a.subtask_id
 from 
-    cte a
-where not exists 
-    (select 1 from Executed b 
-        where a.task_id = b.task_id and a.subtask_id = b.subtask_id)
+    cte a 
+left join 
+    Executed b 
+on 
+    a.task_id = b.task_id and a.subtask_id = b.subtask_id
+where 
+    b.subtask_id is null 
