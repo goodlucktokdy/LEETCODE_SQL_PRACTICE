a49297802b9304f101b4966b8267f55da6ba75f9
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-03 17:24:48 +0900

    Time: 1160 ms (47.34%) | Memory: 0B (100.00%) - LeetSync

--- 2313-longest-winning-streak/longest-winning-streak.sql
+++ 2313-longest-winning-streak/longest-winning-streak.sql
@@ -1,29 +1,30 @@
+# Write your MySQL query statement below
 with base as (
     select 
         player_id,
-        count(distinct match_day) as streaks
-    from (
-        select 
-            player_id,
-            match_day,
-            result,
-            row_number() over (partition by player_id order by match_day) - row_number() over (partition by player_id,result order by match_day) as sess
-        from 
-            Matches
-    ) a 
-    where 
-        result = 'Win'
-    group by 
-        player_id, sess
+        match_day,
+        result,
+        row_number() over (partition by player_id order by match_day) - row_number() over (partition by player_id, result order by match_day) as sess
+    from 
+        Matches
 )
 select 
     a.player_id as player_id,
     max(coalesce(b.streaks,0)) as longest_streak
 from 
     Matches a 
-left join
-    base b
-on
+left join (
+    select 
+        player_id,
+        count(distinct match_day) as streaks
+    from 
+        base
+    where 
+        result = 'Win'
+    group by 
+        player_id, sess
+) b 
+on 
     a.player_id = b.player_id
 group by 
-    player_id 
+    player_id
