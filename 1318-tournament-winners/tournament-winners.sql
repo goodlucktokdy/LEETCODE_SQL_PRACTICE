d4a6e63862e67458a332f40bd810b1a4aba0c99c
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-03 15:26:22 +0900

    Time: 814 ms (87.92%) | Memory: 0B (100.00%) - LeetSync

--- 1318-tournament-winners/tournament-winners.sql
+++ 1318-tournament-winners/tournament-winners.sql
@@ -1,5 +1,5 @@
-
-with base as (
+# Write your MySQL query statement below
+with pairs as (
     select 
         first_player as player_id,
         first_score as score
@@ -12,10 +12,10 @@ with base as (
     from 
         Matches
 ),
-group_info as (
+rank_info as (
     select 
-        player_id,
         group_id,
+        player_id,
         dense_rank() over (partition by group_id order by total_score desc, player_id asc) as ranks
     from (
         select 
@@ -23,19 +23,19 @@ group_info as (
             b.group_id,
             sum(a.score) as total_score
         from 
-            base a 
+            pairs a 
         inner join 
             Players b 
         on 
-            a.player_id = b.player_id
+            a.player_id = b.player_id 
         group by 
             a.player_id, b.group_id
-    ) a
+    ) a 
 )
 select 
     group_id,
     player_id
 from 
-    group_info 
+    rank_info 
 where 
-    ranks = 1 
+    ranks = 1
