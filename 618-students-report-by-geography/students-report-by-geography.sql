52ac9179d0e18709e5b57a2dbfb941b40eeb6eed
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-04 10:27:49 +0900

    Time: 446 ms (50.87%) | Memory: 0B (100.00%) - LeetSync

--- 618-students-report-by-geography/students-report-by-geography.sql
+++ 618-students-report-by-geography/students-report-by-geography.sql
@@ -1,13 +1,12 @@
-
-
+# Write your MySQL query statement below
 select 
     max(case when continent = 'America' then name else null end) as America,
     max(case when continent = 'Asia' then name else null end) as Asia,
     max(case when continent = 'Europe' then name else null end) as Europe
 from (
     select 
-        name,
         continent,
+        name,
         row_number() over (partition by continent order by name) as rnums
     from 
         Student
