0596afc0eabf917abc109a70caa06c668abfafa9
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-04 10:21:22 +0900

    Time: 1365 ms (5.05%) | Memory: 0B (100.00%) - LeetSync

--- 601-human-traffic-of-stadium/human-traffic-of-stadium.sql
+++ 601-human-traffic-of-stadium/human-traffic-of-stadium.sql
@@ -1,30 +1,35 @@
 # Write your MySQL query statement below
-with sess_info as (
+with base as (
     select 
         id,
         visit_date,
-        sess,
         people,
-        count(id) over (partition by sess) as cnts
+        cast(row_number() over (order by id) as real) - id as sess
     from (
         select 
             id,
             visit_date,
-            people,
-            cast(row_number() over (order by id) as real) - id as sess
+            people
         from 
             Stadium
         where 
             people >= 100
-    ) a 
+    ) a
 )
 select 
     id,
     visit_date,
     people
-from 
-    sess_info 
+from (
+    select 
+        id,
+        visit_date,
+        people,
+        count(id) over (partition by sess) as cnts
+    from 
+        base 
+) a 
 where 
     cnts >= 3
 order by 
-    visit_date
+    visit_date asc 
