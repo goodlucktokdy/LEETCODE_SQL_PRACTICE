8e950d79697ea833aef2cbae0ae5a0309f72db04
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-04 10:40:44 +0900

    Time: 2886 ms (15.83%) | Memory: 0B (100.00%) - LeetSync

--- 1269-market-analysis-ii/market-analysis-ii.sql
+++ 1269-market-analysis-ii/market-analysis-ii.sql
@@ -1,10 +1,9 @@
 # Write your MySQL query statement below
-with sales_info as (
+with base as (
     select 
-        a.order_date,
-        b.item_brand,
         a.seller_id,
-        row_number() over (partition by a.seller_id order by a.order_date) as rnums
+        dense_rank() over (partition by a.seller_id order by a.order_date) as rnums,
+        b.item_brand
     from 
         Orders a 
     inner join 
@@ -15,18 +14,9 @@ with sales_info as (
 select 
     a.user_id as seller_id,
     if(b.item_brand is null,'no','yes') as 2nd_item_fav_brand
-from
-    Users a
-left join (
-    select 
-        order_date,
-        item_brand,
-        seller_id
-    from 
-        sales_info a
-    where exists 
-        (select 1 from Users b 
-            where a.seller_id = b.user_id and a.rnums = 2 and a.item_brand = b.favorite_brand)
-) b 
+from 
+    Users a 
+left join 
+    base b 
 on 
-    a.user_id = b.seller_id
+    a.user_id = b.seller_id and b.rnums = 2 and a.favorite_brand = b.item_brand
