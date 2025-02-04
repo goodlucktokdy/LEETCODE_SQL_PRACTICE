8ff614a12e3a915473f0556b4fa4fffe4e9a40dc
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-04 12:07:50 +0900

    Time: 613 ms (33.43%) | Memory: 0B (100.00%) - LeetSync

--- 3302-viewers-turned-streamers/viewers-turned-streamers.sql
+++ 3302-viewers-turned-streamers/viewers-turned-streamers.sql
@@ -1,24 +1,24 @@
 # Write your MySQL query statement below
-with base as (
+with sequence as (
     select 
         user_id,
         session_start,
         session_end,
         session_id,
         session_type,
-        row_number() over (partition by user_id order by session_start asc) as rnums
+        row_number() over (partition by user_id order by session_start, session_end) as rnums
     from 
-        Sessions 
+        Sessions
 )
 select 
     user_id,
     count(distinct session_id) as sessions_count
 from 
-    base
+    sequence 
 where 
-    user_id in (select user_id from base where rnums = 1 and session_type = 'Viewer')
-    and session_type = 'Streamer' 
+    user_id in (select user_id from sequence where rnums = 1 and session_type = 'Viewer')
+    and session_type = 'Streamer'
 group by 
-    user_id
+    user_id 
 order by 
-    sessions_count desc, user_id desc
+    sessions_count desc, user_id desc 
