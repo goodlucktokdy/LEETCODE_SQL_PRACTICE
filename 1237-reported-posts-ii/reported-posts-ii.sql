15ef4a24878d68cf9eb3fa8cf5e8f07a6a6cf39d
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-06 20:28:34 +0900

    Time: 1513 ms (62.55%) | Memory: 0B (100.00%) - LeetSync

--- 1237-reported-posts-ii/reported-posts-ii.sql
+++ 1237-reported-posts-ii/reported-posts-ii.sql
@@ -2,9 +2,8 @@
 with base as (
     select 
         a.post_id,
-        a.action_date,
-        a.action,
         a.extra,
+        a.action_date,
         b.remove_date
     from 
         Actions a 
@@ -12,17 +11,17 @@ with base as (
         Removals b 
     on 
         a.post_id = b.post_id
-    where 
-        a.action = 'report' and a.extra = 'spam'
+    where
+        a.extra = 'spam'
 )
 select 
-    round(avg(a.removals),2) as average_daily_percent
+    round(100.0 * avg(removed),2) as average_daily_percent
 from (
-    select
+    select 
         action_date,
-        coalesce(100.0*count(distinct case when remove_date is not null then post_id else null end)/count(distinct post_id),0) as removals
+        count(distinct case when remove_date is not null then post_id else null end)/count(distinct post_id) as removed
     from 
-        base
+        base 
     group by 
-        1
+        action_date
 ) a 
