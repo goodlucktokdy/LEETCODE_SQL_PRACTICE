a6b8788505c920252c9d24e50fe681e89346e517
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-06 19:46:12 +0900

    Time: 913 ms (65.17%) | Memory: 0B (100.00%) - LeetSync

--- 1163-project-employees-iii/project-employees-iii.sql
+++ 1163-project-employees-iii/project-employees-iii.sql
@@ -1,23 +1,19 @@
-with base as (
-    select
-        b.project_id,
+# Write your MySQL query statement below
+select 
+    project_id,
+    employee_id
+from (
+    select 
+        a.project_id,
         a.employee_id,
-        max(a.experience_years) over (partition by b.project_id) as max_years
+        b.experience_years,
+        dense_rank() over (partition by a.project_id order by b.experience_years desc) as ranks
     from 
-        Employee a
-    left join
-        Project b
-    on
-        a.employee_id = b.employee_id
-)
-select
-    a.project_id,
-    b.employee_id
-from 
-    base a 
-inner join
-    Employee b
-on
-    a.employee_id = b.employee_id
-where
-    b.experience_years = a.max_years and a.project_id is not null
+        Project a 
+    inner join 
+        Employee b 
+    on 
+        a.employee_id = b.employee_id 
+) a 
+where 
+    ranks = 1
