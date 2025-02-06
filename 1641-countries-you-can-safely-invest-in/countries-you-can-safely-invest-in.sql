fc7fe44cf9047abe698d1ff94accc1ed2a679490
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-06 23:30:39 +0900

    Time: 3128 ms (5.80%) | Memory: 0B (100.00%) - LeetSync

--- 1641-countries-you-can-safely-invest-in/countries-you-can-safely-invest-in.sql
+++ 1641-countries-you-can-safely-invest-in/countries-you-can-safely-invest-in.sql
@@ -1,51 +1,56 @@
-with person_country as (
-    select
-        a.id,
-        a.name,
-        left(a.phone_number,3) as country_code,
-        b.name as country_name
+# Write your MySQL query statement below
+with base as (
+    select 
+        b.id,
+        b.name,
+        a.name as country
     from 
-        Person a 
-    inner join 
-        Country b
-    on 
-        left(a.phone_number,3) = b.country_code
-)
-, caller_callee as (
-    select
-        a.caller_id,
-        a.callee_id,
-        a.duration,
-        b.country_name as caller_state,
-        c.country_name as callee_state
-    from 
-        person_country b
-    inner join 
-        Calls a
-    on 
-        a.caller_id = b.id
-    inner join
-        person_country c
-    on 
-        a.callee_id = c.id
-), mid_cte as (
-    select
-        caller_state as country,
+        Country a 
+    inner join (
+        select 
+            id,
+            name,
+            left(phone_number,3) as country_code
+        from 
+            Person
+    ) b 
+    on
+        a.country_code = b.country_code
+),
+pairs as (
+    select 
+        caller_id,
+        callee_id,
         duration
     from 
-        caller_callee
+        Calls
     union all
-    select
-        callee_state as country,
+    select 
+        callee_id,
+        caller_id,
         duration
     from 
-        caller_callee
+        Calls
 )
-select
-    country
-from 
-    mid_cte
-group by 
-    country
-having 
-    avg(duration) > (select avg(duration) from mid_cte)
+select 
+    distinct
+    callers as country
+from (
+    select 
+        a.country as callers,
+        duration,
+        avg(b.duration) over () as global_avg,
+        avg(b.duration) over (partition by a.country) as country_avg
+    from 
+        base a 
+    inner join 
+        pairs b 
+    on 
+        a.id = b.caller_id
+    inner join 
+        base c 
+    on 
+        b.callee_id = c.id
+) a
+where 
+    country_avg > global_avg
