b800057adeef25c38714668af1c020a359116968
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-06 18:40:41 +0900

    Time: 459 ms (73.17%) | Memory: 0B (100.00%) - LeetSync

--- 178-rank-scores/rank-scores.sql
+++ 178-rank-scores/rank-scores.sql
@@ -1,3 +1,8 @@
-  select score, dense_rank() over (order by score desc) as 'rank'
-        from Scores
-    
+# Write your MySQL query statement below
+select 
+    score,
+    dense_rank() over (order by score desc) as 'rank'
+from 
+    Scores
+order by 
+    score desc
