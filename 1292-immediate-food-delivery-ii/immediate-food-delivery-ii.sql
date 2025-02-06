b884d75110a9d72d6a934e1a375e01d024423647
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-06 21:01:02 +0900

    Time: 700 ms (86.95%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,24 +1,14 @@
-with base as 
-(
-    select 
-        a.delivery_id,
-        a.customer_id,
-        case when a.customer_pref_delivery_date = a.first_order_date then 'immediate' else 'scheduled'
-        end as schedules
-    from (
-        select 
-            delivery_id,
-            customer_id,
-            min(order_date) over (partition by customer_id) as first_order_date,
-            order_date,
-            customer_pref_delivery_date
-        from 
-            Delivery
-    ) a
-    where 
-        a.first_order_date = a.order_date
-)
+# Write your MySQL query statement below
 select 
-    round(100.0*count(distinct case when schedules = 'immediate' then customer_id end)/count(distinct customer_id),2) as immediate_percentage
-from 
-    base 
+    round(100.0*count(distinct case when order_date = customer_pref_delivery_date then customer_id else null end)/count(distinct customer_id),2) as immediate_percentage
+from (
+    select 
+        customer_id,
+        min(order_date) over (partition by customer_id) as first_order,
+        order_date,
+        customer_pref_delivery_date
+    from   
+        Delivery
+) a 
+where 
+    first_order = order_date
