8a9ff35c9ef1276b2fb222b34fc85468ae07efe3
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-06 19:26:13 +0900

    Time: 640 ms (86.46%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,22 +1,19 @@
 # Write your MySQL query statement below
 with base as (
-    select
-        b.customer_id,
-        sum(distinct a.product_key) as sums,
-        count(distinct a.product_key) as cnts
+    select 
+        customer_id,
+        sum(distinct product_key) as sums,
+        count(distinct product_key) as cnts
     from 
-        Product a 
-    join 
-        Customer b 
-    on
-        a.product_key = b.product_key
+        Customer
     group by 
-        b.customer_id
+        customer_id
 )
 select 
+    distinct 
     customer_id
 from 
-    base
+    base 
 where 
-    sums = (select sum(distinct product_key) from Product)
-    and cnts = (select count(distinct product_key) from Product)
+    sums = (select sum(distinct product_key) from Product) and 
+    cnts = (select count(distinct product_key) from Product)
