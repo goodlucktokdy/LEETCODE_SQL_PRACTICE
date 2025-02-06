3a6a02d4c5d17bc4efa2adabaa75278a1416af94
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-06 20:18:28 +0900

    Time: 799 ms (86.76%) | Memory: 0B (100.00%) - LeetSync

--- 1225-active-businesses/active-businesses.sql
+++ 1225-active-businesses/active-businesses.sql
@@ -1,26 +1,18 @@
 # Write your MySQL query statement below
-with base as (
-    select 
-        a.business_id
-    from (
-        select 
-            distinct
-            business_id,
-            event_type,
-            occurrences,
-            avg(occurrences) over (partition by event_type) as avg_occurrences_in_event
-        from 
-            Events
-    ) a 
-    where 
-        occurrences > avg_occurrences_in_event 
-)
 select 
-    distinct 
     business_id
-from 
-    base
+from (
+    select 
+        business_id,
+        event_type,
+        occurrences,
+        avg(occurrences) over (partition by event_type) as avg_occurrences
+    from 
+        Events
+) a 
+where 
+    occurrences > avg_occurrences
 group by 
     business_id
 having 
-    count(*) > 1
+    count(distinct event_type) > 1
