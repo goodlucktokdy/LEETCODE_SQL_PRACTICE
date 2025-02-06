d721fe9e9610fa3271b7986331a80c40d4cb163f
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-06 22:52:17 +0900

    Time: 852 ms (57.73%) | Memory: 0B (100.00%) - LeetSync

--- 1523-capital-gainloss/capital-gainloss.sql
+++ 1523-capital-gainloss/capital-gainloss.sql
@@ -1,15 +1,8 @@
-with base as (
-    select
-        stock_name,
-        operation_day,
-        price,
-        sum(case when operation = 'Buy' then -1 * price else price end) over (partition by stock_name order by operation_day) as capital_gain_loss
-    from 
-        Stocks
-)
-select
-    distinct
+# Write your MySQL query statement below
+select 
     stock_name,
-    first_value(capital_gain_loss) over (partition by stock_name order by operation_day desc) as capital_gain_loss
+    sum(case when operation = 'Buy' then -1 * price else price end) as capital_gain_loss
 from 
-    base
+    Stocks
+group by 
+    stock_name
