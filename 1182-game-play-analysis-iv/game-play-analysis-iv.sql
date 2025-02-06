cb06fe68015e33880f452639e8144e1921d34ab8
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-06 18:51:06 +0900

    Time: 690 ms (84.66%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,18 +1,11 @@
 # Write your MySQL query statement below
-with base as (
-    select 
-        a.player_id,
-        timestampdiff(day,a.first_day, a.event_date) as diff_of_day
-    from (
-        select 
-            player_id,
-            min(event_date) over (partition by player_id) as first_day,
-            event_date
-        from 
-            Activity
-    ) a
-)
 select 
-    round(count(distinct case when diff_of_day = 1 then player_id else null end)/count(distinct player_id),2) as fraction
-from 
-    base
+    round(count(distinct case when date_add(first_visit,interval 1 day) = event_date then player_id else null end)/count(distinct player_id),2) as fraction
+from (
+    select 
+        player_id,
+        min(event_date) over (partition by player_id) as first_visit,
+        event_date
+    from 
+        Activity
+) a 
