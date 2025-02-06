3f7a552692345cee3a6176fc9d8cbe8badef1582
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-06 19:19:26 +0900

    Time: 841 ms (14.75%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,17 +1,11 @@
 # Write your MySQL query statement below
-select 
-    case when max_id + 1 = new_id and max_id % 2 = 1 then 
-    a.new_id - 1 else a.new_id end as id,
-    a.student
-from (
-    select 
-        id,
-        student,
-        case when id % 2 = 1 then id + 1 
-        else id - 1 end as new_id,
-        max(id) over () as max_id
-    from 
-        Seat
-) a
+select
+    case when 
+        (select max(id) from Seat) = id and id % 2 then id 
+        when id % 2 then id + 1
+        else id - 1 end as id,
+    student
+from 
+    Seat
 order by 
-    id
+    id 
