1a308491b8de465f1ef97bd31f5731f6a7cee3e2
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-06 23:03:05 +0900

    Time: 1581 ms (12.90%) | Memory: 0B (100.00%) - LeetSync

--- 1536-customers-who-bought-products-a-and-b-but-not-c/customers-who-bought-products-a-and-b-but-not-c.sql
+++ 1536-customers-who-bought-products-a-and-b-but-not-c/customers-who-bought-products-a-and-b-but-not-c.sql
@@ -1,33 +1,38 @@
+# Write your MySQL query statement below
 with base as (
-    select
-        customer_id
-    from
-        Orders
-    where 
-        product_name = 'C'
-), mid_cte as (
-    select
-        distinct
-        customer_id,
-        case when product_name = 'A' then 1 else null end as A,
-        case when product_name = 'B' then 1 else null end as B
+    select 
+        a.customer_id,
+        a.customer_name,
+        b.product_name
     from 
-        Orders
-    where
-        customer_id not in (select * from base)
+        Customers a 
+    inner join 
+        Orders b 
+    on 
+        a.customer_id = b.customer_id
 )
-select
-    a.customer_id,
-    b.customer_name
-from
-    mid_cte a 
-inner join
-    Customers b
-on
-    a.customer_id = b.customer_id
+select 
+    customer_id,
+    customer_name
+from (
+    select 
+        a.customer_id,
+        a.customer_name,
+        a.product_name
+    from 
+        base a 
+    where not exists 
+        (select 1 from base b 
+            where a.customer_id = b.customer_id and b.product_name = 'C')
+    and exists
+        (select 1 from base b 
+            where a.customer_id = b.customer_id and b.product_name in ('A','B'))
+) a 
+where 
+    product_name in ('A','B')
 group by 
-    1,2
+    customer_id,customer_name 
 having 
-    max(A) = 1 and max(B) = 1
+    count(distinct product_name) = 2
 order by 
-    a.customer_id
+    customer_id
