2eaa6747471437b2d675bc5ae7637bdda1e9578a
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-06 21:58:02 +0900

    Time: 1402 ms (44.70%) | Memory: 0B (100.00%) - LeetSync

--- 1339-team-scores-in-football-tournament/team-scores-in-football-tournament.sql
+++ 1339-team-scores-in-football-tournament/team-scores-in-football-tournament.sql
@@ -1,31 +1,41 @@
+# Write your MySQL query statement below
 with base as (
-    select
-        host_team as team,
+    select 
+        host_team,
+        guest_team,
         case when host_goals > guest_goals then 3 
-            when host_goals = guest_goals then 1
-            else 0 end as score
+            when host_goals = guest_goals then 1 
+            else 0 end as host_score,
+        case when guest_goals > host_goals then 3 
+            when guest_goals = host_goals then 1 
+            else 0 end as guest_score
     from 
         Matches
-    union all
-    select
-        guest_team as team,
-        case when host_goals < guest_goals then 3
-            when host_goals = guest_goals then 1
-                else 0 end as score
-    from 
-        Matches
-)
-select
-    ifnull(b.team,a.team_id) as team_id,
-    a.team_name,
-    ifnull(sum(b.score),0) as num_points
+),
+score_cte as (
+select 
+    host_team as team,
+    host_score as score
 from 
-    Teams a
+    base 
+union all
+select 
+    guest_team as team,
+    guest_score as score
+from 
+    base 
+)
+select 
+    a.team_id,
+    a.team_name,
+    sum(coalesce(b.score,0)) as num_points 
+from 
+    Teams a 
 left join
-    base b
-on
+    score_cte b 
+on 
     a.team_id = b.team
-group by
-    1,2
-order by
-    num_points desc, team_id asc
+group by 
+    a.team_id, a.team_name
+order by 
+    num_points desc, a.team_id asc 
