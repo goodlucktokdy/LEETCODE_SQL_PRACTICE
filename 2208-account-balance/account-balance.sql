004d5b6a33cd493ea196c0fdf8853029ea227439
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-07 11:05:37 +0900

    Time: 1015 ms (30.70%) | Memory: 0B (100.00%) - LeetSync

--- 2208-account-balance/account-balance.sql
+++ 2208-account-balance/account-balance.sql
@@ -1,21 +1,9 @@
 # Write your MySQL query statement below
-with base as (
-    select 
-        a.account_id,
-        b.day,
-        b.type,
-        if(b.type = 'Deposit',b.amount,-1 * b.amount) as amount
-    from 
-        Transactions a 
-    left join 
-        Transactions b 
-    on 
-        a.account_id = b.account_id and a.day = b.day and a.type = b.type
-)
 select 
-    distinct
     account_id,
     day,
-    sum(amount) over (partition by account_id order by day asc) as balance
-from   
-    base
+    sum(case when type = 'Deposit' then amount else -1 * amount end) over (partition by account_id order by day) as balance
+from 
+    Transactions
+order by 
+    account_id, day
