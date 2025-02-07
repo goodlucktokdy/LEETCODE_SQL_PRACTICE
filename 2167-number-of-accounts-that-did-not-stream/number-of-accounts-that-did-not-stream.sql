496a8dc1e81c032b4e9eb76a6c1cad0a5b5fcf28
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-07 10:48:12 +0900

    Time: 1460 ms (60.68%) | Memory: 0B (100.00%) - LeetSync

--- 2167-number-of-accounts-that-did-not-stream/number-of-accounts-that-did-not-stream.sql
+++ 2167-number-of-accounts-that-did-not-stream/number-of-accounts-that-did-not-stream.sql
@@ -1,19 +1,12 @@
 # Write your MySQL query statement below
-with base as (
-    select 
-        a.account_id,
-        b.start_date,
-        a.stream_date
-    from 
-        Streams a 
-    left join 
-        Subscriptions b 
-    on 
-        a.account_id = b.account_id
-    where 
-        (year(b.end_date) = 2021 or year(b.start_date) = 2021) and year(a.stream_date) != 2021
-)
 select 
-    count(distinct account_id) as accounts_count
+    count(distinct a.account_id) as accounts_count
 from 
-    base
+    Subscriptions a 
+left join 
+    Streams b 
+on 
+    a.account_id = b.account_id and year(b.stream_date) = 2021
+where
+    2021 between year(a.start_date) and year(a.end_date)
+    and b.account_id is null
