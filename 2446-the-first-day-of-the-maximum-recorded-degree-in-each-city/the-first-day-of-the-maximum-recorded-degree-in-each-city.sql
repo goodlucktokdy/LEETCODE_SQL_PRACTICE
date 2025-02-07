beee2f268cdb704c885f12fd8bbf683615dd93e4
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-07 15:38:36 +0900

    Time: 1091 ms (30.95%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2446-the-first-day-of-the-maximum-recorded-degree-in-each-city/the-first-day-of-the-maximum-recorded-degree-in-each-city.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+select 
+    city_id,
+    day,
+    degree
+from (
+    select
+        city_id,
+        day,
+        degree,
+        dense_rank() over (partition by city_id order by degree desc, day asc) as ranks
+    from 
+        Weather
+) a
+where 
+    ranks = 1
+order by 
+    city_id
