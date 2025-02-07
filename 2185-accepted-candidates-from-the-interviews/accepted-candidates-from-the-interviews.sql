78d734e009873518d05009bceedfd27864324bb6
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-07 10:53:00 +0900

    Time: 2122 ms (42.80%) | Memory: 0B (100.00%) - LeetSync

--- 2185-accepted-candidates-from-the-interviews/accepted-candidates-from-the-interviews.sql
+++ 2185-accepted-candidates-from-the-interviews/accepted-candidates-from-the-interviews.sql
@@ -1,34 +1,22 @@
 # Write your MySQL query statement below
-with base as (
+select 
+    candidate_id
+from (
     select 
         a.candidate_id,
         a.years_of_exp,
-        b.interview_id,
-        b.round_id,
-        b.score
+        a.interview_id,
+        sum(b.score) as score
     from 
         Candidates a 
-    left join 
+    inner join 
         Rounds b 
     on 
         a.interview_id = b.interview_id
-)
-,final_info as (
-    select 
-        candidate_id,
-        interview_id,
-        sum(score) as total_score
-    from 
-        base
     where 
-        years_of_exp >= 2
+        a.years_of_exp >= 2
     group by 
-        1,2
+        a.candidate_id, a.interview_id, a.years_of_exp
     having 
-        sum(score) > 15
-)
-select 
-    distinct 
-    candidate_id
-from 
-    final_info
+        score > 15
+) a
