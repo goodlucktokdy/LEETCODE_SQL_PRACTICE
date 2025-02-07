35bc090c4d70a8f1a8c54d44b36f97a06da91d13
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-07 15:41:19 +0900

    Time: 1808 ms (22.81%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2451-product-sales-analysis-iv/product-sales-analysis-iv.sql
@@ -0,0 +1,28 @@
+# Write your MySQL query statement below
+with base as (
+    select 
+        user_id,
+        product_id,
+        dense_rank() over (partition by user_id order by price desc) as ranks
+    from (
+        select 
+            a.user_id,
+            a.product_id,
+            sum(a.quantity * b.price) as price
+        from 
+            Sales a
+        inner join 
+            Product b 
+        on 
+            a.product_id = b.product_id
+        group by 
+            a.user_id, a.product_id
+    ) a
+)
+select 
+    user_id,
+    product_id
+from 
+    base 
+where 
+    ranks = 1
