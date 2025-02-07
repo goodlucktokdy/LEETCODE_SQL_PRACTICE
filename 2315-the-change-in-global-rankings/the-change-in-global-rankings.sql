49b3c6866aad8a63e783f0fde6b9d0a7869a4267
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-07 14:37:41 +0900

    Time: 1395 ms (44.19%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2315-the-change-in-global-rankings/the-change-in-global-rankings.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+select 
+    team_id,
+    name,
+    cast(og_ranks as real) - cast(new_ranks as real) as rank_diff
+from (
+    select 
+        a.team_id,
+        a.name,
+        dense_rank() over (order by a.points desc, a.name asc) as og_ranks,
+        dense_rank() over (order by a.points + b.points_change desc, a.name asc) as new_ranks
+    from 
+        TeamPoints a 
+    inner join 
+        PointsChange b 
+    on 
+        a.team_id = b.team_id
+) a
