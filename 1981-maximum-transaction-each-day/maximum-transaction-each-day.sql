303a7cb2ffaba714bf8572e6308106fe1f346bbc
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-07 11:47:58 +0900

    Time: 779 ms (78.81%) | Memory: 0B (100.00%) - LeetSync

--- 1981-maximum-transaction-each-day/maximum-transaction-each-day.sql
+++ 1981-maximum-transaction-each-day/maximum-transaction-each-day.sql
@@ -1,19 +1,16 @@
-with daily_amount as (
-    select
-        day,
-        max(amount) as max_amount_daily
+# Write your MySQL query statement below
+select 
+    transaction_id
+from (
+    select 
+        date(day) as dates,
+        transaction_id,
+        amount,
+        dense_rank() over (partition by date(day) order by amount desc) as ranks
     from 
         Transactions
-    group by 
-        day
-)
-select
-    b.transaction_id
-from 
-    daily_amount a
-inner join
-    Transactions b
-on
-    a.max_amount_daily = b.amount and a.day = b.day
-order by
-    b.transaction_id asc
+) a 
+where 
+    ranks = 1
+order by 
+    transaction_id
