cf8d4ec05a9fff9ab6ccf3d3cf970442d84159e2
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-08 15:38:40 +0900

    Time: 542 ms (54.46%) | Memory: 0B (100.00%) - LeetSync

--- 3285-manager-of-the-largest-department/manager-of-the-largest-department.sql
+++ 3285-manager-of-the-largest-department/manager-of-the-largest-department.sql
@@ -2,23 +2,27 @@
 with base as (
     select 
         dep_id,
-        dense_rank() over (order by count(*) desc) as ranks
-    from 
-        Employees
-    group by 
-        dep_id
+        dense_rank() over (order by emp_num desc) as ranks
+    from (
+        select 
+            dep_id,
+            count(distinct emp_id) as emp_num
+        from 
+            Employees 
+        group by 
+            dep_id
+    ) a
 )
 select 
-    distinct
-    b.emp_name as manager_name,
-    a.dep_id
+    a.emp_name as manager_name,
+    b.dep_id
 from 
-    base a
+    Employees a 
 inner join 
-    Employees b 
+    base b 
 on 
-    a.dep_id = b.dep_id
+    a.dep_id = b.dep_id and b.ranks = 1
 where 
-    a.ranks = 1 and b.position = 'Manager'
+    a.position = 'manager'
 order by 
-    a.dep_id asc
+    b.dep_id
