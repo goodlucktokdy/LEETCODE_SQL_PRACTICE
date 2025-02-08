c36922e821778d043de2518cc19827359a6314da
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-08 20:26:50 +0900

    Time: 556 ms (50.00%) | Memory: 0B (100.00%) - LeetSync

--- 3473-calculate-parking-fees-and-duration/calculate-parking-fees-and-duration.sql
+++ 3473-calculate-parking-fees-and-duration/calculate-parking-fees-and-duration.sql
@@ -1,29 +1,31 @@
 # Write your MySQL query statement below
 with base as (
     select 
-        a.lot_id,
-        a.car_id,
-        sum(a.diff_of_minutes)/60 as duration,
-        sum(a.fee_paid) as total_paid
+        car_id,
+        lot_id,
+        diff_seconds,
+        total_fee_paid,
+        dense_rank() over (partition by car_id order by diff_seconds desc) as lot_ranks
     from (
         select 
-            lot_id,
             car_id,
-            fee_paid,
-            timestampdiff(minute,entry_time,exit_time) as diff_of_minutes
+            lot_id,
+            sum(timestampdiff(second,entry_time,exit_time)) as diff_seconds,
+            sum(fee_paid) as total_fee_paid
         from 
             ParkingTransactions
+        group by 
+            car_id, lot_id
     ) a
-    group by 
-        1,2
 )
 select 
-    distinct
     car_id,
-    round(sum(total_paid) over (partition by car_id),2) as total_fee_paid,
-    round(sum(total_paid) over (partition by car_id)/sum(duration) over (partition by car_id),2) as avg_hourly_fee,
-    first_value(lot_id) over (partition by car_id order by duration desc) as most_time_lot
+    round(sum(total_fee_paid),2) as total_fee_paid,
+    round(sum(total_fee_paid)/(sum(diff_seconds)/3600),2) as avg_hourly_fee,
+    max(case when lot_ranks = 1 then lot_id else null end) as most_time_lot
 from 
-    base
+    base 
+group by 
+    car_id 
 order by 
     car_id
