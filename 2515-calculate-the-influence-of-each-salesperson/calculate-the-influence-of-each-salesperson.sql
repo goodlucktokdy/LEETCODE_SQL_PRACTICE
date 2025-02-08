8091845711a1f15b22ecd5a72d02103a0a221054
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-08 13:23:34 +0900

    Time: 1801 ms (63.86%) | Memory: 0B (100.00%) - LeetSync

--- 2515-calculate-the-influence-of-each-salesperson/calculate-the-influence-of-each-salesperson.sql
+++ 2515-calculate-the-influence-of-each-salesperson/calculate-the-influence-of-each-salesperson.sql
@@ -1,8 +1,13 @@
 # Write your MySQL query statement below
-with base as (
+select 
+    a.salesperson_id,
+    a.name,
+    coalesce(b.total,0) as total
+from 
+    Salesperson a 
+left join (
     select 
         a.salesperson_id,
-        a.customer_id,
         sum(b.price) as total
     from 
         Customer a 
@@ -11,17 +16,7 @@ with base as (
     on 
         a.customer_id = b.customer_id
     group by 
-        a.salesperson_id, a.customer_id
-)
-select 
-    a.salesperson_id,
-    a.name,
-    sum(coalesce(b.total,0)) as total
-from 
-    Salesperson a 
-left join 
-    base b 
+        a.salesperson_id
+) b
 on 
     a.salesperson_id = b.salesperson_id
-group by 
-    a.salesperson_id, a.name
