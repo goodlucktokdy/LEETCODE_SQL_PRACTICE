7c124fd910aabd90bb7991e1d02d1c4ab0b350d6
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-08 14:51:41 +0900

    Time: 552 ms (38.50%) | Memory: 0B (100.00%) - LeetSync

--- 3156-rolling-average-steps/rolling-average-steps.sql
+++ 3156-rolling-average-steps/rolling-average-steps.sql
@@ -1,20 +1,38 @@
+# Write your MySQL query statement below
 with base as (
     select 
         user_id,
-        steps_count,
         steps_date,
-        avg(steps_count) over (partition by user_id order by steps_date rows between 2 preceding and current row) as rolling_average,
-        lag(steps_date,2) over (partition by user_id order by steps_date) as prev_date
-    from 
-        Steps
+        steps_count,
+        sess,
+        cast(row_number() over (partition by user_id, sess order by steps_date) as real) as rnums,
+        count(steps_date) over (partition by sess) as cnts
+    from (
+        select 
+            user_id,
+            steps_date,
+            date_sub(steps_date, interval row_number() over (partition by user_id order by steps_date) day) as sess,
+            steps_count
+        from 
+            Steps
+    ) a
 )
 select 
     user_id,
     steps_date,
-    round(rolling_average,2) as rolling_average
-from 
-    base 
+    rolling_average
+from (
+    select 
+        user_id,
+        steps_date,
+        rnums,
+        round(avg(steps_count) over (partition by user_id, sess order by rnums range between 2 preceding and current row),2) as rolling_average 
+    from 
+        base 
+    where 
+        cnts >= 3
+) a
 where 
-    datediff(steps_date,prev_date) + 1 = 3
+    rnums >= 3
 order by 
     user_id, steps_date
