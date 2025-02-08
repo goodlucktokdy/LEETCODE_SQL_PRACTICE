0ccdc0d7548e72fbdc1961a26c2a356b5ab597b0
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-08 15:43:40 +0900

    Time: 708 ms (47.95%) | Memory: 0B (100.00%) - LeetSync

--- 3300-friday-purchases-i/friday-purchases-i.sql
+++ 3300-friday-purchases-i/friday-purchases-i.sql
@@ -1,20 +1,13 @@
 # Write your MySQL query statement below
 select 
-    extract(week from a.purchase_date) - extract(week from '2023-11-01') + 1 as week_of_month,
-    a.purchase_date,
-    sum(a.amount_spend) as total_amount
-from (
-    select
-        user_id,
-        purchase_date,
-        amount_spend
-    from 
-        Purchases 
-    where 
-        year(purchase_date) = 2023 and month(purchase_date) = 11
-        and weekday(purchase_date) = 4
-) a 
+    week(purchase_date) - week('2023-11-01') + 1 as week_of_month,
+    purchase_date,
+    sum(amount_spend) as total_amount
+from 
+    Purchases
+where 
+    weekday(purchase_date) = 4
 group by 
-    1,2
+    week_of_month, purchase_date
 order by 
     week_of_month 
