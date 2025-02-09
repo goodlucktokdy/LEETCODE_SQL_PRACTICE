4d2cc6cf347897ab49f4651cd58b1646904f8881
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-09 15:02:56 +0900

    Time: 955 ms (78.91%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,27 +1,20 @@
-with base as (
-    select
+# Write your MySQL query statement below
+select 
+    Department,
+    Employee,
+    Salary
+from (
+    select 
         b.name as Department,
         a.name as Employee,
-        a.salary
+        a.salary as Salary,
+        dense_rank() over (partition by a.departmentId order by a.salary desc) as ranks
     from 
-        Employee a
-    left join
-        Department b
+        Employee a 
+    inner join 
+        Department b 
     on 
-        b.id = a.departmentId
-)
-select
-    a.Department,
-    a.Employee,
-    a.Salary
-from (
-    select
-        Department,
-        Employee,
-        Salary,
-        dense_rank() over (partition by Department order by salary desc) as ranks
-    from 
-        base
+        a.departmentId = b.id
 ) a
 where 
     ranks = 1
