09a8799d93ade639ffde4cf31db3f35574d7937c
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-09 16:10:56 +0900

    Time: 1484 ms (70.11%) | Memory: 0B (100.00%) - LeetSync

--- 1237-reported-posts-ii/reported-posts-ii.sql
+++ 1237-reported-posts-ii/reported-posts-ii.sql
@@ -1,27 +1,18 @@
 # Write your MySQL query statement below
-with base as (
+select 
+    round(avg(100.0*avg_percent),2) as average_daily_percent
+from (
     select 
-        a.post_id,
-        a.extra,
         a.action_date,
-        b.remove_date
+        count(distinct case when a.extra = 'spam' then b.post_id else null end)/count(distinct a.post_id) as avg_percent
     from 
         Actions a 
     left join 
         Removals b 
     on 
-        a.post_id = b.post_id
-    where
-        a.extra = 'spam'
-)
-select 
-    round(100.0 * avg(removed),2) as average_daily_percent
-from (
-    select 
-        action_date,
-        count(distinct case when remove_date is not null then post_id else null end)/count(distinct post_id) as removed
-    from 
-        base 
+        a.post_id = b.post_id 
+    where 
+        a.extra = 'spam' 
     group by 
-        action_date
-) a 
+        a.action_date
+) a
