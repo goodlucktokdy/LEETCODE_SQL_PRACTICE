473fc58df3c34e0024b6b6f40bf9225bd928872a
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-09 16:18:33 +0900

    Time: 2054 ms (59.33%) | Memory: 0B (100.00%) - LeetSync

--- 1268-market-analysis-i/market-analysis-i.sql
+++ 1268-market-analysis-i/market-analysis-i.sql
@@ -1,13 +1,13 @@
 # Write your MySQL query statement below
-select
+select 
     a.user_id as buyer_id,
-    a.join_date as join_date,
-    count(b.order_id) as orders_in_2019
+    a.join_date,
+    count(distinct case when year(b.order_date) = 2019 then b.order_id else null end) as orders_in_2019
 from 
     Users a 
 left join 
     Orders b 
 on 
-    a.user_id = b.buyer_id and year(b.order_date) = 2019 
+    a.user_id = b.buyer_id
 group by 
-    a.user_id
+    a.user_id, a.join_date
