d9f2a5c6febb3fb7029943b0d23e5960e7191f63
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-09 15:17:31 +0900

    Time: 484 ms (46.60%) | Memory: 0B (100.00%) - LeetSync

---
+++ 578-get-highest-answer-rate-question/get-highest-answer-rate-question.sql
@@ -0,0 +1,21 @@
+# Write your MySQL query statement below
+with base as (
+    select 
+        question_id,
+        dense_rank() over (order by answer_rate desc, question_id asc) as ranks
+    from (
+        select 
+            question_id,
+            count(case when action = 'answer' then question_id else null end)/count(case when action = 'show' then question_id else null end) as answer_rate
+        from 
+            SurveyLog
+        group by 
+            question_id
+    ) a
+)
+select 
+    question_id as survey_log
+from 
+    base 
+where 
+    ranks = 1
