0d787c1be965029f47c2c00eacbb516cecf9b943
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-09 16:34:45 +0900

    Time: 1375 ms (67.30%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,29 +1,18 @@
 # Write your MySQL query statement below
-with base as (
-    select 
-        person_name,
-        cume_weight,
-        turn
-    from (
-        select 
-            person_name,
-            turn,
-            sum(weight) over (order by turn) as cume_weight,
-            1000 as limits
-        from 
-            Queue
-    ) a 
-    where 
-        cume_weight <= limits
-)
 select 
     person_name
 from (
     select 
+        person_id,
         person_name,
-        dense_rank() over (order by turn desc) as ranks
+        turn,
+        sum(weight) over (order by turn) as cume_weight,
+        1000 as limits
     from 
-        base 
+        Queue
 ) a
 where 
-    ranks = 1
+    cume_weight <= limits
+order by 
+    turn desc
+limit 1
