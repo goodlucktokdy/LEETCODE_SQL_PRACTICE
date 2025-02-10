697cc59569dfec3004f28cbafc538d897d6dfc80
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-10 12:31:54 +0900

    Time: 561 ms (93.21%) | Memory: 0B (100.00%) - LeetSync

--- 1981-maximum-transaction-each-day/maximum-transaction-each-day.sql
+++ 1981-maximum-transaction-each-day/maximum-transaction-each-day.sql
@@ -1,15 +1,15 @@
 # Write your MySQL query statement below
 select 
+    distinct
     transaction_id
 from (
     select 
-        date(day) as dates,
         transaction_id,
-        amount,
-        dense_rank() over (partition by date(day) order by amount desc) as ranks
+        day,
+        dense_rank() over (partition by day order by amount desc) as ranks
     from 
         Transactions
-) a 
+) a
 where 
     ranks = 1
 order by 
