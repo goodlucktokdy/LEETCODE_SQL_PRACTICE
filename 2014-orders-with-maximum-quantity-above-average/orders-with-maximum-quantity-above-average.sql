5197fa0aed014e8814cf56a817deca5f5d46a817
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-10 12:56:46 +0900

    Time: 1459 ms (22.22%) | Memory: 0B (100.00%) - LeetSync

--- 2014-orders-with-maximum-quantity-above-average/orders-with-maximum-quantity-above-average.sql
+++ 2014-orders-with-maximum-quantity-above-average/orders-with-maximum-quantity-above-average.sql
@@ -2,24 +2,23 @@
 with base as (
     select 
         order_id,
-        max(quantity) as max_quantity,
-        sum(quantity)/count(product_id) as avg_quantity
-    from
-        OrdersDetails
-    group by 
-        order_id
+        order_avg,
+        max_quantity
+    from (
+        select 
+            order_id,
+            max(quantity) as max_quantity,
+            sum(quantity)/count(distinct product_id) as order_avg
+        from 
+            OrdersDetails
+        group by 
+            order_id
+    ) a
 )
 select 
     distinct 
     order_id
-from (
-    select 
-        order_id,
-        max_quantity,
-        avg_quantity,
-        max(avg_quantity) over () as max_avg
-    from 
-        base
-) a 
+from 
+    base 
 where 
-    max_quantity > max_avg
+    max_quantity > (select max(order_avg) from base)
