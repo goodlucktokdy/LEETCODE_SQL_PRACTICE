6799fc9d9d4f5a7bc5bf67f479e1039f6dad18dc
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-10 12:20:01 +0900

    Time: 504 ms (97.65%) | Memory: 0B (100.00%) - LeetSync

--- 1898-leetflex-banned-accounts/leetflex-banned-accounts.sql
+++ 1898-leetflex-banned-accounts/leetflex-banned-accounts.sql
@@ -1,18 +1,11 @@
 # Write your MySQL query statement below
 select 
-    distinct 
-    account_id
-from (
-    select 
-        a.account_id,
-        a.ip_address,
-        a.login,
-        a.logout
-    from 
-        LogInfo a 
-    inner join 
-        LogInfo b 
-    on 
-        a.account_id = b.account_id and a.ip_address != b.ip_address and
-        (a.login <= b.login and b.login <= a.logout)
-) a 
+    distinct
+    a.account_id
+from 
+    LogInfo a 
+inner join 
+    LogInfo b 
+on 
+    a.account_id = b.account_id and a.ip_address != b.ip_address
+    and (a.login <= b.login and b.login <= a.logout)
