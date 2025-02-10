944d5d66b4ea7afb59f31aea46b4c3c6fa2b1167
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-10 13:04:47 +0900

    Time: 2911 ms (39.47%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -1,29 +1,24 @@
 # Write your MySQL query statement below
-with categories as (
-    select 
-        'Low Salary' as category 
-    union 
-    select 
-        'Average Salary' as category
-    union 
+with base as (
     select 
         'High Salary' as category
+    union all
+    select 
+        'Average Salary' as category
+    union all
+    select 
+        'Low Salary' as category
 )
 select 
     a.category,
-    count(distinct b.account_id) as accounts_count
+    count(distinct account_id) as accounts_count
 from 
-    categories a 
-left join (
-   select 
-        case when income < 20000 then 'Low Salary'
-            when income between 20000 and 50000 then 'Average Salary'
-            else 'High Salary' end as category,
-        account_id
-    from 
-        Accounts
-) b
+    base a 
+left join 
+    Accounts b 
 on 
-    a.category = b.category
+    a.category = case when b.income > 50000 then 'High Salary'
+        when b.income between 20000 and 50000 then 'Average Salary'
+        else 'Low Salary' end
 group by 
-    a.category 
+    a.category
