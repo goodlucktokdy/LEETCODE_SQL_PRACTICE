088b1817f45bad5119a7474c889736310d69f278
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-10 13:18:58 +0900

    Time: 1071 ms (38.15%) | Memory: 0B (100.00%) - LeetSync

--- 2143-count-the-number-of-experiments/count-the-number-of-experiments.sql
+++ 2143-count-the-number-of-experiments/count-the-number-of-experiments.sql
@@ -1,34 +1,34 @@
 # Write your MySQL query statement below
-with base as (
-select 
-    'Android' as platform,
-    'Reading' as experiment_name
-union 
-select 
-    'IOS' as platform,
-    'Sports' as experiment_name
-union 
-select 
-    'Web' as platform,
-    'Programming' as experiment_name
+with base_og as (
+    select 
+        'IOS' as platform,
+        'Reading' as experiment_name
+    union all
+    select
+        'Android' as platform,
+        'Programming' as experiment_name
+    union all
+    select 
+        'Web' as platform,
+        'Sports' as experiment_name
 ),
-pairs as (
+base as (
     select 
         a.platform,
         b.experiment_name
     from 
-        base a 
+        base_og a 
     cross join 
-        base b
+        base_og b
 )
 select 
     a.platform,
     a.experiment_name,
     count(distinct b.experiment_id) as num_experiments
 from 
-    pairs a 
+    base a
 left join 
-    Experiments b 
+    Experiments b
 on 
     a.platform = b.platform and a.experiment_name = b.experiment_name
 group by 
