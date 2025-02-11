512f4e4e5bf8e943e3cd0b44effd0e2f63454cff
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-11 12:05:29 +0900

    Time: 796 ms (55.38%) | Memory: 0B (100.00%) - LeetSync

--- 2441-arrange-table-by-gender/arrange-table-by-gender.sql
+++ 2441-arrange-table-by-gender/arrange-table-by-gender.sql
@@ -1,17 +1,25 @@
 # Write your MySQL query statement below
-select 
-    user_id,
-    gender
-from (
+with base as (
     select 
         user_id,
         gender,
-        case when gender = 'female' then 1 
-            when gender = 'other' then 2
-            else 3 end as gender_rn,
-        row_number() over (partition by gender order by user_id) as rnums
-    from 
-        Genders
-) a
+        gen_ranks,
+        row_number() over (partition by gen_ranks order by user_id) as rnums
+    from (
+        select 
+            user_id,
+            gender,
+            case when gender = 'female' then 1 
+                when gender = 'other' then 2
+                else 3 end as gen_ranks
+        from 
+            Genders
+    ) a
+)
+select 
+    user_id,
+    gender
+from 
+    base 
 order by 
-    rnums, gender_rn
+    rnums asc, gen_ranks asc
