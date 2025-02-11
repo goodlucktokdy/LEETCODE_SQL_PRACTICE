27d0ae765af49534e84ae894aa4d193ac3d456ee
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-11 12:29:50 +0900

    Time: 1305 ms (86.32%) | Memory: 0B (100.00%) - LeetSync

--- 2534-employees-with-deductions/employees-with-deductions.sql
+++ 2534-employees-with-deductions/employees-with-deductions.sql
@@ -1,20 +1,27 @@
 # Write your MySQL query statement below
+with base as (
+    select 
+        employee_id,
+        ceil(sum(timestampdiff(second,in_time,out_time)) / 60) as duration
+    from 
+        Logs
+    group by 
+        employee_id
+)
 select 
     distinct 
     employee_id
 from (
     select 
         a.employee_id,
-        ceil(sum(coalesce(timestampdiff(second,b.in_time,b.out_time),0)) / 60)  as diff_minute,
-        a.needed_hours
+        a.needed_hours,
+        coalesce(b.duration,0) as duration
     from 
         Employees a 
     left join 
-        Logs b 
+        base b 
     on 
-        a.employee_id = b.employee_id
-    group by 
-        a.employee_id, a.needed_hours
-) a 
+        a.employee_id = b.employee_id 
+) a
 where 
-    diff_minute < needed_hours * 60
+    needed_hours * 60 > duration 
