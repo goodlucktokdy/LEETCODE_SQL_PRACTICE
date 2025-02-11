5b8ad280e62b4dc5851006de5aaf47f727aab557
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-11 11:51:04 +0900

    Time: 1056 ms (80.93%) | Memory: 0B (100.00%) - LeetSync

--- 2315-the-change-in-global-rankings/the-change-in-global-rankings.sql
+++ 2315-the-change-in-global-rankings/the-change-in-global-rankings.sql
@@ -2,16 +2,16 @@
 select 
     team_id,
     name,
-    cast(og_ranks as real) - cast(new_ranks as real) as rank_diff
+    cast(og_ranks as real)- cast(new_ranks as real) as rank_diff
 from (
     select 
         a.team_id,
         a.name,
         dense_rank() over (order by a.points desc, a.name asc) as og_ranks,
-        dense_rank() over (order by a.points + b.points_change desc, a.name asc) as new_ranks
+        dense_rank() over (order by a.points + coalesce(b.points_change) desc, a.name asc) as new_ranks
     from 
         TeamPoints a 
-    inner join 
+    left join 
         PointsChange b 
     on 
         a.team_id = b.team_id
