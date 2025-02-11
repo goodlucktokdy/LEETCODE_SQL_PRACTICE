e27393bf5adff4cc465f059ee8f3bba2c3d74726
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-11 11:59:01 +0900

    Time: 464 ms (89.45%) | Memory: 0B (100.00%) - LeetSync

--- 2440-tasks-count-in-the-weekend/tasks-count-in-the-weekend.sql
+++ 2440-tasks-count-in-the-weekend/tasks-count-in-the-weekend.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 select 
-    count(distinct case when weekday(submit_date) in (5,6) then task_id else null end) as weekend_cnt,
-    count(distinct case when weekday(submit_date) in (0,1,2,3,4) then task_id else null end) as working_cnt
+    count(case when weekday(submit_date) in (5,6) then task_id else null end) as weekend_cnt,
+    count(case when weekday(submit_date) in (0,1,2,3,4) then task_id else null end) as working_cnt
 from 
     Tasks
