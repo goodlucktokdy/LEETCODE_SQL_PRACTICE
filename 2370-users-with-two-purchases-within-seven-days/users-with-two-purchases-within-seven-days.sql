31e12fd44391e315f4a7f65225e8886c6292c1a2
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-11 11:53:15 +0900

    Time: 617 ms (80.21%) | Memory: 0B (100.00%) - LeetSync

--- 2370-users-with-two-purchases-within-seven-days/users-with-two-purchases-within-seven-days.sql
+++ 2370-users-with-two-purchases-within-seven-days/users-with-two-purchases-within-seven-days.sql
@@ -1,15 +1,13 @@
 # Write your MySQL query statement below
-
 select 
     distinct
-    a.user_id
+    a.user_id as user_id
 from 
     Purchases a 
 inner join 
     Purchases b 
 on 
-    a.user_id = b.user_id and a.purchase_id != b.purchase_id
+    a.user_id = b.user_id and a.purchase_id != b.purchase_id 
     and abs(timestampdiff(day,a.purchase_date,b.purchase_date)) <= 7
 order by 
-    a.user_id
-
+    user_id
