4f137ce27a0c41e1a53419a95443afe8bbf6a3aa
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-11 12:32:53 +0900

    Time: 893 ms (73.48%) | Memory: 0B (100.00%) - LeetSync

--- 2840-immediate-food-delivery-iii/immediate-food-delivery-iii.sql
+++ 2840-immediate-food-delivery-iii/immediate-food-delivery-iii.sql
@@ -1,10 +1,10 @@
 # Write your MySQL query statement below
-select 
+select
     order_date,
     round(100.0*count(distinct case when order_date = customer_pref_delivery_date then delivery_id else null end)/count(distinct delivery_id),2) as immediate_percentage
 from 
-    Delivery 
+    Delivery
 group by 
-    order_date 
+    order_date
 order by 
     order_date
