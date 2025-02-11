b89304631a63c8b61024cb39e65a6f8a207417ac
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-11 12:43:09 +0900

    Time: 570 ms (35.75%) | Memory: 0B (100.00%) - LeetSync

--- 3156-rolling-average-steps/rolling-average-steps.sql
+++ 3156-rolling-average-steps/rolling-average-steps.sql
@@ -1,20 +1,19 @@
 # Write your MySQL query statement below
 with base as (
+    select
+        user_id,
+        steps_count,
+        steps_date,
+        sess,
+        dense_rank() over (partition by user_id,sess order by steps_date) as rnums
+    from (
     select 
         user_id,
-        steps_date,
         steps_count,
-        sess,
-        cast(row_number() over (partition by user_id, sess order by steps_date) as real) as rnums,
-        count(steps_date) over (partition by sess) as cnts
-    from (
-        select 
-            user_id,
-            steps_date,
-            date_sub(steps_date, interval row_number() over (partition by user_id order by steps_date) day) as sess,
-            steps_count
-        from 
-            Steps
+        steps_date,
+        date_sub(steps_date,interval dense_rank() over (partition by user_id order by steps_date) day) as sess
+    from 
+        Steps
     ) a
 )
 select 
@@ -26,11 +25,10 @@ from (
         user_id,
         steps_date,
         rnums,
-        round(avg(steps_count) over (partition by user_id, sess order by rnums range between 2 preceding and current row),2) as rolling_average 
+        round(avg(steps_count) over (partition by user_id,sess order by steps_date range between 
+            interval 2 day preceding and current row),2) as rolling_average
     from 
         base 
-    where 
-        cnts >= 3
 ) a
 where 
     rnums >= 3
