1d01b5dc9c3dd1405b0a673348c9d05eaee86261
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-11 11:47:56 +0900

    Time: 627 ms (84.58%) | Memory: 0B (100.00%) - LeetSync

--- 2302-order-two-columns-independently/order-two-columns-independently.sql
+++ 2302-order-two-columns-independently/order-two-columns-independently.sql
@@ -1,24 +1,12 @@
 # Write your MySQL query statement below
-with c1 as (
-    select 
-        first_col,
-        row_number() over (order by first_col) as rn 
-    from Data
-),
-c2 as (
-    select 
-        second_col,
-        row_number() over (order by second_col desc) as rn 
-    from Data
-)
 select 
-    a.first_col,
-    b.second_col
+    first_col,
+    second_col
 from 
-    c1 a 
+    (select first_col,row_number() over (order by first_col) as fr from Data) a
 inner join 
-    c2 b 
+    (select second_col,row_number() over (order by second_col desc) as sr from Data) b
 on 
-    a.rn = b.rn
+    a.fr = b.sr
 order by 
-    a.rn
+    a.fr
