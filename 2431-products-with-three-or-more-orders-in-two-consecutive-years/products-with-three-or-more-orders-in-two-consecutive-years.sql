7f622ff193f81056ac3cdb468ff1218d716bc1d7
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-11 11:57:22 +0900

    Time: 698 ms (81.72%) | Memory: 0B (100.00%) - LeetSync

--- 2431-products-with-three-or-more-orders-in-two-consecutive-years/products-with-three-or-more-orders-in-two-consecutive-years.sql
+++ 2431-products-with-three-or-more-orders-in-two-consecutive-years/products-with-three-or-more-orders-in-two-consecutive-years.sql
@@ -1,27 +1,29 @@
 # Write your MySQL query statement below
 with base as (
     select 
+        year,
         product_id,
-        lag(years,1,years-1) over (partition by product_id order by years) as prev_years,
-        years,
-        lag(orders,1,0) over (partition by product_id order by years) as prev_orders,
-        orders
+        orders,
+        year - cast(row_number() over (partition by product_id order by year) as real) as sess
     from (
         select 
+            year(purchase_date) as year,
             product_id,
-            year(purchase_date) as years,
             count(distinct order_id) as orders
         from 
             Orders 
         group by 
-            product_id, years
+            year(purchase_date), product_id
     ) a
+    where 
+        orders >= 3
 )
 select 
     distinct 
-    a.product_id
+    product_id
 from 
-    base a 
-where exists 
-    (select 1 from base b 
-        where a.product_id = b.product_id and (b.years - b.prev_years = 1 and b.prev_orders >= 3 and b.orders >= 3))
+    base 
+group by 
+    product_id, sess
+having 
+    count(distinct year) >= 2
