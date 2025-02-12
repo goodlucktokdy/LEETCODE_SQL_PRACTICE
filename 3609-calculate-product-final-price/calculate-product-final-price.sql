d694391b7034be091fe1dea8cf23fd564cd8fe7d
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-12 14:52:37 +0900

    Time: 1028 ms (37.13%) | Memory: 0B (100.00%) - LeetSync

--- 3609-calculate-product-final-price/calculate-product-final-price.sql
+++ 3609-calculate-product-final-price/calculate-product-final-price.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 select 
-    a.product_id,
-    case when b.category is not null then a.price * (100 - b.discount) * 0.01 else a.price end as final_price,
+    a.product_id as product_id,
+    a.price * (100-coalesce(b.discount,0)) * 0.01 as final_price,
     a.category
 from 
     Products a 
@@ -10,4 +10,4 @@ left join
 on 
     a.category = b.category
 order by 
-    a.product_id asc 
+    product_id
