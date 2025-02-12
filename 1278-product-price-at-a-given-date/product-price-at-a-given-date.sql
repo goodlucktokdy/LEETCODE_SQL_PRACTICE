8f256271373128ea979421e14b28a2078fb0b786
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-12 19:51:26 +0900

    Time: 710 ms (79.08%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,26 +1,46 @@
-select 
-    product_id,
-    10 as price
-from 
-    Products
-group by 
-    product_id
-having 
-    min(change_date) > '2019-08-16'
-union all
-select 
-    product_id,
-    new_price as price
-from 
-    Products
-where
-    (product_id, change_date) in (
-        select product_id,
-            max(change_date)
+with base as (
+    select 
+        distinct
+        product_id,
+        new_price as price
+    from (
+        select 
+            product_id,
+            new_price,
+            change_date,
+            max(change_date) over (partition by product_id) as max_date
         from 
             Products
-        where
-            change_date <=' 2019-08-16'
-        group by 
-            product_id
-    )
+        where 
+            change_date <= '2019-08-16'
+    ) a
+    where 
+        max_date = change_date
+),
+other_cte as (    
+    select 
+        product_id,
+        10 as price
+    from (
+        select 
+            product_id,
+            new_price,
+            change_date
+        from 
+            Products a 
+        where not exists 
+            (select 1 from Products b 
+                where a.product_id = b.product_id and b.change_date <= '2019-08-16')
+    ) c
+)
+select 
+    product_id,
+    price
+from 
+    base 
+union 
+select 
+    product_id,
+    price
+from 
+    other_cte
