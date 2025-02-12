3db785949f6d82a3adb8bbf2b8470cc18f863fa5
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-12 20:44:12 +0900

    Time: 1436 ms (19.46%) | Memory: 0B (100.00%) - LeetSync

--- 1193-game-play-analysis-v/game-play-analysis-v.sql
+++ 1193-game-play-analysis-v/game-play-analysis-v.sql
@@ -1,23 +1,15 @@
 # Write your MySQL query statement below
-with install_info as (
-    select 
-        install_dt,
-        count(distinct player_id) as installs,
-        count(distinct case when date_add(install_dt,interval 1 day) = event_date then player_id else null end) as retention
-    from (
-        select 
-            player_id,
-            min(event_date) over (partition by player_id) as install_dt,
-            event_date
-        from 
-            Activity
-    ) a 
-    group by 
-        install_dt
-)
 select 
     install_dt,
-    installs,
-    round(retention/installs,2) as Day1_retention
-from 
-    install_info 
+    count(distinct player_id) as installs,
+    round(count(distinct case when date_add(install_dt,interval 1 day) = event_date then player_id else null end)/count(distinct player_id),2) as Day1_retention
+from (
+    select 
+        player_id,
+        event_date,
+        min(event_date) over (partition by player_id) as install_dt
+    from 
+        Activity
+) a
+group by 
+    install_dt
