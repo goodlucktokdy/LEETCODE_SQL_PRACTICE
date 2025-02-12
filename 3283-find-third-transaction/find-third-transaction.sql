8eadc024cfc3c1ad7bb0bc5722e44f5310f33f5a
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-12 13:39:54 +0900

    Time: 816 ms (78.86%) | Memory: 0B (100.00%) - LeetSync

--- 3283-find-third-transaction/find-third-transaction.sql
+++ 3283-find-third-transaction/find-third-transaction.sql
@@ -2,37 +2,21 @@
 with base as (
     select 
         user_id,
+        spend,
         transaction_date,
-        row_number() over (partition by user_id order by transaction_date) as rnums,
-        spend
+        lag(spend) over (partition by user_id order by transaction_date) as prev_spend,
+        lag(spend,2) over (partition by user_id order by transaction_date) as 2prev_spend,
+        row_number() over (partition by user_id order by transaction_date) as rnums
     from 
         Transactions 
 )
 select 
     user_id,
-    third_transaction_spend,
-    third_transaction_date
-from (
-    select 
-        a.user_id,
-        a.transaction_date,
-        a.spend,
-        a.rnums,
-        b.transaction_date as third_transaction_date,
-        b.spend as third_transaction_spend
-    from 
-        base a 
-    left join 
-        base b 
-    on 
-        a.user_id = b.user_id and a.rnums < 3 and b.rnums = 3
-        and a.spend < b.spend
-    where 
-        b.user_id is not null
-) a
-group by 
-    1,2,3
-having 
-    sum(distinct rnums) = 3 
+    spend as third_transaction_spend,
+    transaction_date as third_transaction_date
+from 
+    base
+where 
+    rnums = 3 and prev_spend < spend and 2prev_spend < spend 
 order by 
     user_id
