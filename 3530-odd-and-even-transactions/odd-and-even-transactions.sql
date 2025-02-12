f8706290c2afcc805570f966c32ce50d4c7e538b
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-12 14:29:10 +0900

    Time: 4566 ms (5.08%) | Memory: 0B (100.00%) - LeetSync

--- 3530-odd-and-even-transactions/odd-and-even-transactions.sql
+++ 3530-odd-and-even-transactions/odd-and-even-transactions.sql
@@ -4,7 +4,7 @@ select
     sum(case when amount % 2 = 1 then amount else 0 end) as odd_sum,
     sum(case when amount % 2 = 0 then amount else 0 end) as even_sum
 from 
-    transactions
+    transactions 
 group by 
     transaction_date
 order by 
