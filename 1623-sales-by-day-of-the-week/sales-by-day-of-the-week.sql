4bd763c827a42f433eb559881d786ee014811cd9
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-12 20:55:56 +0900

    Time: 1250 ms (15.89%) | Memory: 0B (100.00%) - LeetSync

--- 1623-sales-by-day-of-the-week/sales-by-day-of-the-week.sql
+++ 1623-sales-by-day-of-the-week/sales-by-day-of-the-week.sql
@@ -1,30 +1,28 @@
 # Write your MySQL query statement below
-with sales_info as (
-    select 
-        a.item_category as Category,
-        weekday(b.order_date) as days,
-        sum(coalesce(b.quantity,0)) as quantity
-    from 
-        Items a 
-    left join 
-        Orders b 
-    on 
-        a.item_id = b.item_id
-    group by 
-        Category, days
-)
 select 
-    Category,
+    item_category as Category,
     max(case when days = 0 then quantity else 0 end) as Monday,
     max(case when days = 1 then quantity else 0 end) as Tuesday,
     max(case when days = 2 then quantity else 0 end) as Wednesday,
     max(case when days = 3 then quantity else 0 end) as Thursday,
     max(case when days = 4 then quantity else 0 end) as Friday,
     max(case when days = 5 then quantity else 0 end) as Saturday,
-    max(case when days = 6 then quantity else 0 end) as Sunday 
-from 
-    sales_info
+    max(case when days = 6 then quantity else 0 end) as Sunday
+from (
+    select 
+        a.item_category,
+        weekday(b.order_date) as days,
+        sum(coalesce(b.quantity,0)) as quantity
+    from 
+        Items a
+    left join 
+        Orders b 
+    on 
+        a.item_id = b.item_id
+    group by 
+        a.item_category, weekday(b.order_date)
+) c
 group by 
-    Category
+    item_category
 order by 
-    Category 
+    Category
