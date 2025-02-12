22d27587e158bfea9c637190d751f82466d43b4a
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-12 14:06:15 +0900

    Time: 788 ms (75.74%) | Memory: 0B (100.00%) - LeetSync

--- 3432-find-longest-calls/find-longest-calls.sql
+++ 3432-find-longest-calls/find-longest-calls.sql
@@ -1,30 +1,22 @@
 # Write your MySQL query statement below
-with base as (
-    select 
-        first_name,
-        type,
-        date_format(sec_to_time(duration),'%H:%i:%S') as duration_formatted,
-        dense_rank() over (partition by type order by duration desc) as ranks
-    from (
-        select 
-            a.first_name,
-            b.type,
-            b.duration as duration
-        from 
-            Contacts a
-        inner join 
-            Calls b 
-        on 
-            a.id = b.contact_id
-    ) a
-)
 select 
     first_name,
     type,
-    duration_formatted
-from 
-    base 
+    date_format(sec_to_time(duration),'%H:%i:%S') as duration_formatted
+from (
+    select 
+        a.first_name,
+        b.type,
+        dense_rank() over (partition by b.type order by b.duration desc) as ranks,
+        b.duration
+    from 
+        Contacts a 
+    inner join 
+        Calls b 
+    on 
+        a.id = b.contact_id
+) a
 where 
     ranks <= 3
 order by 
-    type desc, duration_formatted desc, first_name desc 
+    type desc, duration desc, first_name desc
