25437c2f761bdf239fd874e1143305abc59055c8
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-12 13:47:17 +0900

    Time: 761 ms (41.77%) | Memory: 0B (100.00%) - LeetSync

--- 3300-friday-purchases-i/friday-purchases-i.sql
+++ 3300-friday-purchases-i/friday-purchases-i.sql
@@ -4,10 +4,10 @@ select
     purchase_date,
     sum(amount_spend) as total_amount
 from 
-    Purchases
+    Purchases 
 where 
     weekday(purchase_date) = 4
 group by 
-    week_of_month, purchase_date
+    week(purchase_date) - week('2023-11-01') + 1, purchase_date
 order by 
-    week_of_month 
+    week_of_month
