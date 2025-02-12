d8d694ca93c0225cc61424746d1b5d15b06baa9b
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-12 16:16:26 +0900

    Time: 573 ms (91.30%) | Memory: 0B (100.00%) - LeetSync

--- 585-investments-in-2016/investments-in-2016.sql
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -1,27 +1,18 @@
 # Write your MySQL query statement below
-with base as (
-    select 
-        distinct
-        a.pid,
-        a.tiv_2016,
-        a.lat,
-        a.lon
-    from 
-        Insurance a 
-    inner join 
-        Insurance b 
-    on 
-        a.pid != b.pid and a.tiv_2015 = b.tiv_2015
-)
 select 
     round(sum(tiv_2016),2) as tiv_2016
 from (
     select 
+        distinct
         pid,
         tiv_2016
-    from
-        base a 
-    where not exists 
+    from 
+        Insurance a 
+    where exists
+        (select 1 from Insurance b 
+            where a.pid != b.pid and a.tiv_2015 = b.tiv_2015)
+    and 
+        not exists
         (select 1 from Insurance b 
             where a.pid != b.pid and (a.lat,a.lon) = (b.lat,b.lon))
-) a
+) c
