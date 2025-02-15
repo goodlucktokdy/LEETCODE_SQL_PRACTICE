c5e32a06d6713644b1700b1afecb4fb7d974dcf2
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-15 19:51:20 +0900

    Time: 1246 ms (78.83%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,27 +1,16 @@
 # Write your MySQL query statement below
-with base as (
-    select 
-        person_id,
-        rnums,
-        max(rnums) over () as max_rnums
-    from (
-        select 
-            person_id,
-            person_name,
-            turn,
-            row_number() over (order by turn) as rnums,
-            sum(weight) over (order by turn) as cume_weight
-        from 
-            Queue
-    ) a
-    where 
-        cume_weight <= 1000
-)
 select 
-    a.person_name
-from 
-    Queue a 
-inner join 
-    base b 
-on 
-    a.person_id = b.person_id and b.max_rnums = b.rnums
+    person_name
+from (
+    select 
+        person_name,
+        sum(weight) over (order by turn) as cume_weight,
+        turn
+    from 
+        Queue
+) a
+where 
+    cume_weight <= 1000
+order by 
+    turn desc 
+limit 1
