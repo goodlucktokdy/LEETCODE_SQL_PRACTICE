ade7e4bc3bc90011c0663c459374787c7e3f7649
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-15 19:46:17 +0900

    Time: 1071 ms (62.27%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,14 +1,12 @@
 # Write your MySQL query statement below
 select 
-    round(100.0*count(distinct case when order_date = customer_pref_delivery_date then customer_id else null end)/count(distinct customer_id),2) as immediate_percentage
+    round(100.0*count(distinct case when first_order = customer_pref_delivery_date then customer_id else null end)/count(distinct customer_id),2) as immediate_percentage
 from (
     select 
+        delivery_id,
         customer_id,
-        order_date,
-        dense_rank() over (partition by customer_id order by order_date asc) as ranks,
+        min(order_date) over (partition by customer_id) as first_order,
         customer_pref_delivery_date
     from 
         Delivery
 ) a
-where 
-    ranks = 1
