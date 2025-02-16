5243b70f30442da17e14a7d5337f265bf061f91d
김동윤(Kim Dongyoon) <148170828+goodlucktokdy@users.noreply.github.com>
2025-02-16 12:53:00 +0900

    Time: 2225 ms (21.93%) | Memory: 0B (100.00%) - LeetSync

--- 1862-count-apples-and-oranges/count-apples-and-oranges.sql
+++ 1862-count-apples-and-oranges/count-apples-and-oranges.sql
@@ -1,19 +1,17 @@
 # Write your MySQL query statement below
 select 
-    sum(apple_count + chest_apple) as apple_count,
-    sum(orange_count + chest_orange) as orange_count
+    sum(apple_count) as apple_count,
+    sum(orange_count) as orange_count
 from (
     select 
         a.box_id,
-        a.chest_id,
-        a.apple_count,
-        a.orange_count,
-        coalesce(b.apple_count,0) as chest_apple,
-        coalesce(b.orange_count,0) as chest_orange
+        b.chest_id,
+        a.apple_count + coalesce(b.apple_count,0) as apple_count,
+        a.orange_count + coalesce(b.orange_count,0) as orange_count
     from 
         Boxes a 
     left join 
         Chests b 
     on 
-        a.chest_id = b.chest_id 
+        a.chest_id = b.chest_id
 ) a
